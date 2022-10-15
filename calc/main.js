97ce48aff8e039c4349576c47d960bfbb0776536
Askar Khisamov <askarcheg244@gmail.com>
2022-10-15 14:06:42 +0300

    two calcs added

--- calc/main.js
+++ calc/main.js
@@ -1,3 +1,5 @@
+//Калькулятор через switch//
+
 let a = prompt("Первое число");
 let show = prompt("Операция");
 let b = prompt("Второе число");
@@ -6,7 +8,9 @@ function calc(show, a, b) {
   a = Number(a);
   b = Number(b);
 
-  switch (show) {
+  if (isNaN(a) || isNaN(b)) { //Проверка на NaN
+    return 'Вводите только числа';
+  } else switch (show) {
     case "-":
       return a - b;
     case "+":
@@ -14,7 +18,7 @@ function calc(show, a, b) {
     case "*":
       return a * b;
     case "/": {
-      if (b == 0) {
+      if (b == 0) { //Проверка на деление на 0
         alert("На 0 делить нельзя!");
       } else return a / b;
     }
@@ -26,3 +30,33 @@ function calc(show, a, b) {
 }
 
 alert(calc(show, a, b));
+
+//Калькулятор через if//
+
+let c = prompt ('Первое число');
+let option = prompt ('Операция');
+let d = prompt('Второе число');
+
+function calc(option, c, d) {
+
+  a = Number(c);
+  b = Number(d);
+
+  if (isNaN(c) || isNaN(d)) {
+    return 'Вводите только числа';
+  } else if (option == '-') {
+      return (c - d);
+  } else if (option == '+') {
+      return (c + d);
+  } else if (option == '*') {
+      return (c * d);
+  } else if (option == '/') {
+      if (d == 0) {
+          alert ('На 0 делить нельзя!'); 
+      } else return (c / d);
+  } else if (option == '**') {
+      return (c ** d);
+  }
+}
+
+alert(calc(option, c, d));
