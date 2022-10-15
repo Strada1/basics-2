f40ed7a474ada99e61114058b7cca56b9f75f4b8
wayleet <wayleet@yandex.ru>
2022-10-15 17:11:34 +0300

    тестирую коммиты и заливаю их на гитхаб

--- wayleet/main.js
+++ wayleet/main.js
@@ -1,28 +1,30 @@
+//походу я научился пользоваться коммитами и заливать их на гитхаб)))
+
 function sum(num1, num2) {
-	return num1 + num2;
+   return num1 + num2;
 }
 function multi(num1, num2) {
-	return num1 * num2;
+   return num1 * num2;
 }
 function subtract(num1, num2) {
-	return num1 - num2;
+   return num1 - num2;
 }
 
 function calc(operator, num1, num2) {
-	let ans = NaN;
-	switch (operator) {
-		case "sum":
-			ans = sum(num1, num2);
-			break;
-		case "multi":
-			ans = multi(num1, num2);
-			break;
-		case "subtract": {
-			ans = subtract(num1, num2);
-			break;
-		}
-	}
-	return ans;
+   let ans = NaN;
+   switch (operator) {
+      case "sum":
+         ans = sum(num1, num2);
+         break;
+      case "multi":
+         ans = multi(num1, num2);
+         break;
+      case "subtract": {
+         ans = subtract(num1, num2);
+         break;
+      }
+   }
+   return ans;
 }
 let id = prompt('Введи индефикатор нужного действия ("multi", "sum", "subtract"): ', '');
 let a = prompt('Введи 1 число', '');
@@ -30,9 +32,9 @@ let b = prompt('Введи 2 число', '');
 result = calc(id, a, b);
 
 if (isNaN(result)) {
-	alert("Ты ввел что-то не так(");
-	alert("Перезапусти программу и попробуй еще");
+   alert("Ты ввел что-то не так(");
+   alert("Перезапусти программу и попробуй еще");
 }
 else {
-	alert(result);
+   alert(result);
 }
