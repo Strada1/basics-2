bc4e910162bcbe5beb85fa62d830280fbb06ab7e
Alex <Alex.pojinatel97@gmail.com>
2022-10-15 13:18:34 +0300

    replacing variables

---
+++ calcSwitch.js
@@ -0,0 +1,17 @@
+function calc (action, a, b) {
+    switch(action) {
+        case 'add' :
+            return  (a + b);
+        case 'multi' :
+            return  (a * b);
+        case 'subtract' :
+            return  (a - b);
+        default :
+            return ('error')
+    }
+}
+
+console.log(calc ('add', 1, 2))
+console.log(calc ('multi', 1, 2))
+console.log(calc ('subtract', 3, 2))
+console.log(calc ('kavo', 3, 2))
