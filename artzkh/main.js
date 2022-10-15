75ebe6bafcc0931fdbad80c0791af97a9914cfa8
Arteww <arrmf1@mail.ru>
2022-10-16 04:10:11 +0700

    new .js file added

---
+++ artzkh/main.js
@@ -0,0 +1,19 @@
+// console.log("0" + 1 - 1 == 0 + 1 - "1")
+// console.log(null + 1 >= 0 + 1)
+// console.log(undefined !== NaN)
+// console.log(null >= ' ')
+// console.log("ABC" > "ABC ")
+// console.log(" " == "0")
+// console.log(" " >= null)
+
+
+
+console.log(NaN != 0)
+console.log(NaN != 10000)
+console.log(NaN != 'арбуз')
+console.log(NaN != true)
+console.log(NaN != false)
+console.log(NaN != NaN)
+console.log(NaN != undefined)
+
+
