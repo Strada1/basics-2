623db2d7b4e39f70b6a585996d0876d82f0ff089
Suric1 <igorbronko@gmail.com>
2022-10-15 23:17:15 +0300

    Добавлен Калькулятор с использованием Тернарного оператора

--- calc.js
+++ calc.js
@@ -24,6 +24,17 @@ function calc(sign,a,b) {
 }
 
 
+// Калькулятор с использованием Тернарного оператора
+
+function calc(sign,a,b) {
+	return sign === 'add' ? a + b
+		: sign === 'multi' ? a * b
+			: sign === 'subtract' ?  a - b
+				: 'неизвестное значение'
+}
+
+
+
 console.log(calc('add', 1,2))
 console.log(calc('multi', 1,2))
 console.log(calc('subtract', 3,2))
