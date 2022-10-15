1d8992a46abdd2f40e3094342784867741b60081
HolloWiccup <75541143+HolloWiccup@users.noreply.github.com>
2022-10-15 10:46:39 +0300

    Калькулятор с проверкой

--- hollowiccup/ternaryOperator.js
+++ hollowiccup/ternaryOperator.js
@@ -1,23 +1,30 @@
-function calcSum(operator, a, b) {
-    let result = isNaN(a) ? "не разобрал здесь число" :
-                 isNaN(b) ? "не разобрал здесь число" :
-                 operator === 'add' ? +a + +b :
-                 operator === 'multi' ? a * b :
-                 operator === 'subtract' ? a - b :
-                 "вообще не рядом"
-  
-    return result;
-  }
-  
-  console.log(calcSum("add", 1, 2));
-  console.log(calcSum("multi", 1, 2));
-  console.log(calcSum("subtract", 3, 2));
-  console.log(calcSum("", 1, 2));
-  console.log(calcSum(null, 1, 2));
-  console.log(calcSum(true, 1, 2));
-  console.log(calcSum("add", true, 2));
-  console.log(calcSum("add", "1", 2));
-  console.log(calcSum("multi", 1, "2"));
-  console.log(calcSum("subtract", "2", "1"));
-  console.log(calcSum("add", null, 2));
-  console.log(calcSum("add", undefined, 2));
+function calc(operator, a, b) {
+  let result = isValid(a) ? "Число не обнаружено" :
+               isValid(b) ? "Число не обнаружено" :
+               operator === 'add' ? +a + +b :
+               operator === 'multi' ? a * b :
+               operator === 'subtract' ? a - b :
+               "Данное действие не найдено"
+
+  return result;
+}
+function isValid(value){
+  return (value === null ||
+           value === undefined ||
+           value === true ||
+           value === false ||
+           +value === NaN )
+}
+
+console.log(calc("add", 1, 2));
+console.log(calc("multi", 1, 2));
+console.log(calc("subtract", 3, 2));
+console.log(calc("", 1, 2));
+console.log(calc(null, 1, 2));
+console.log(calc(true, 1, 2));
+console.log(calc("add", true, 2));
+console.log(calc("add", "1", 2));
+console.log(calc("multi", 1, "2"));
+console.log(calc("subtract", "2", "1"));
+console.log(calc("add", null, 2));
+console.log(calc("add", undefined, 2));
