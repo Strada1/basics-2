661e39532dcec84e919731f7008f024ef955e43d
Anton <a.orlov1987@gmail.com>
2022-10-15 21:03:21 +0700

    calc switch

---
+++ antonorlov/main.js
@@ -0,0 +1,15 @@
+function calc(ident, a, b) {
+    
+  switch (ident) {
+    case 'add':
+      return a + b;
+    case 'multi':
+      return a * b;
+    case 'subtract':
+      return a - b;
+  }
+};
+
+console.log(calc('add', 1, 2));
+console.log(calc('multi', 1, 2));
+console.log(calc('subtract', 3, 2));
