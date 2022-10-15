ccaf6ef9edf5c979a0a57e63c28a0c3bbd6e227f
zxcprince <graazia322@mail.ru>
2022-10-15 22:29:58 +0300

    first commit

---
+++ calcS.js
@@ -0,0 +1,17 @@
+let a,b,operator,result
+operator="-"
+a=2
+b=3
+switch (operator){
+    case "+":
+    result=a+b;
+    break;
+    case"-":
+    result=a-b;
+    break;
+    case"*":
+    result=a*b;
+    break;
+    }
+    console.log(result);
+    
