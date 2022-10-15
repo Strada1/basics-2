a1ec20cd04f91c2e141321e9e67b953242a49e4c
wayleet <wayleet@yandex.ru>
2022-10-15 16:53:00 +0300

    калькулятор by wayleet

---
+++ wayleet/main.js
@@ -0,0 +1,38 @@
+function sum(num1, num2) {
+	return num1 + num2;
+}
+function multi(num1, num2) {
+	return num1 * num2;
+}
+function subtract(num1, num2) {
+	return num1 - num2;
+}
+
+function calc(operator, num1, num2) {
+	let ans = NaN;
+	switch (operator) {
+		case "sum":
+			ans = sum(num1, num2);
+			break;
+		case "multi":
+			ans = multi(num1, num2);
+			break;
+		case "subtract": {
+			ans = subtract(num1, num2);
+			break;
+		}
+	}
+	return ans;
+}
+let id = prompt('Введи индефикатор нужного действия ("multi", "sum", "subtract"): ', '');
+let a = prompt('Введи 1 число', '');
+let b = prompt('Введи 2 число', '');
+result = calc(id, a, b);
+
+if (isNaN(result)) {
+	alert("Ты ввел что-то не так(");
+	alert("Перезапусти программу и попробуй еще");
+}
+else {
+	alert(result);
+}
