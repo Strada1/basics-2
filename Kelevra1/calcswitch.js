c1bea55bdf0c0d4551a9022ee7649c657586bf4b
Aibek Katenov <a_katenov@kbtu.kz>
2022-10-15 18:15:36 +0600

    changed

--- Kelevra1/calcswitch.js
+++ Kelevra1/calcswitch.js
@@ -1,1 +1,18 @@
-console.log("error");
+function calcsolver(a,b,action){
+    switch(action){
+        case "add":
+            return a+b;
+            break;
+        case "sub":
+            return a-b;
+            break;
+        case "multip":
+            return a*b;
+            break;
+    }
+}
+a = +prompt("vvedite 1 argument");
+b = +prompt("vvedire 2 argument");
+c = prompt("action");
+res = calcsolver(a,b,c);
+alert(res);
