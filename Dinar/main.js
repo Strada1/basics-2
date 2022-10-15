c1e8676677051cf9f1131155a68b4f32b81c25dd
taekmak <dikiaxmadiev@yandex.ru>
2022-10-15 07:48:09 +0300

    first commit

---
+++ Dinar/main.js
@@ -0,0 +1,19 @@
+
+  function culc(id, a, b){
+      switch(id){
+
+            case("add"):
+            alert(a + b);
+            break;
+
+            case("multi"):
+            alert(a * b);
+            break;
+
+            case("subtract"):
+            alert(a - b);
+            break;
+      }
+
+  }
+  culc("add", 89, 65);
