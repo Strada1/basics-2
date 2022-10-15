8fc7665e4970c81b0590dda428e15878d5116be6
Suric1 <igorbronko@gmail.com>
2022-10-15 23:14:12 +0300

    Калькулятор с использованием Условия if / else

---
+++ calc.js
@@ -0,0 +1,13 @@
+// Калькулятор с использованием Условия if / else
+
+function calc(sign,a,b) {
+	if (sign === 'add') return a + b
+	if (sign === 'multi') return a * b
+	if (sign === 'subtract') return a - b
+	else return 'неизвестное значение'
+}
+
+
+
+
+
