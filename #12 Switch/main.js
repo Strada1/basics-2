d88bf30e65f7e7e30fe4e9a22ac6a3cce35bb792
zdarovEzhov <ezhov.95@gmail.com>
2022-10-16 20:28:42 +0300

    test commit

--- #12 Switch/main.js
+++ #12 Switch/main.js
@@ -1,22 +1,25 @@
+const ADD = `add`;
+const MULTI = `multi`;
+const SUBTRACT = `subtract`;
+const USERERRORS = `ooops, ERROR`;
+
 let number1 = +prompt(`first value`, `0`);
 let number2 = +prompt(`second value`, `0`);
 let actionUser = prompt(`choose action (add subtract multi)`, `add`);
 
 function calc(action, arg1, arg2) {
     if (isNaN(arg1) || isNaN(arg2)) {
-        return `работаю только с числами`;
+        return USERERRORS;
     } else switch (action) {
-        case `add`:
+        case ADD:
             return arg1 + arg2;
-        case `multi`:
+        case MULTI:
             return arg1 * arg2;
-        case `subtract`:
+        case SUBTRACT:
             return arg1 - arg2;
         default:
-            return `oops, error`;
+            return USERERRORS;
     }
 }
 
 alert(calc(actionUser, number1, number2));
-
-console.log(calc(`add`, 2, 3));
