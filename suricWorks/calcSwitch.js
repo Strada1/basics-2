be107c2c183a30b80d41a1e724f767ea51456de4
Suric1 <igorbronko@gmail.com>
2022-10-16 23:05:34 +0300

    Калькулятор с использованием Конструкции "switch"

---
+++ suricWorks/calcSwitch.js
@@ -0,0 +1,26 @@
+// Калькулятор с использованием Конструкции "switch"
+
+function calc(sign,a,b) {
+	switch (sign) {
+		case 'add' :
+			return a + bч
+		case 'multi' :
+			return a * b
+		case 'subtract' :
+			return a - b
+		default :
+			return 'неизвестное значение'
+	}
+}
+
+
+
+
+
+
+console.log(calc('add', 1,2))
+console.log(calc('multi', 1,2))
+console.log(calc('subtract', 3,2))
+
+
+console.log(calc('subtractc', 3,2))
