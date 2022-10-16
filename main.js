d675bea38a3d02a76be2e28c71949bd911569faa
Dmytro Kitov <jdimaj@gmail.com>
2022-10-16 16:49:05 +0100

    Now try use git with VS code

--- main.js
+++ main.js
@@ -5,3 +5,12 @@ function calc(action, num1, num2) {
 			break;
 		case 'multi':
 			return num1 * num2;
+			break;
+		case 'substruct':
+			return num1 - num2;
+			break;
+		case 'correct':
+			return num1 / num2;
+			break;
+	}
+}
