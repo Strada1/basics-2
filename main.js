8deeaf67bce0af29cc9911019a2c129edbdbc86f
Dmytro Kitov <jdimaj@gmail.com>
2022-10-16 18:34:13 +0100

    How it is work?

--- main.js
+++ main.js
@@ -1,5 +1,17 @@
- function calc(action, num1, num2){
-  		switch (action) {
-        case 'add':
-          return num1 + num2;
-          break;
+function calc(action, num1, num2) {
+  switch (action) {
+    case 'add':
+      return num1 + num2;
+      break;
+    case 'multi':
+      return num1 * num2;
+      break;
+    case 'substruct':
+      return num1 - num2;
+      break;
+    case 'correct':
+      return num1 / num2;
+      break;
+  }
+}
+console.log("How much time i need to spend this?");
