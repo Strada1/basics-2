37f0da56ae5b381d93536cc28d7fae3b02e8c9db
HoBager <chel43@inbox.ru>
2022-10-18 13:52:24 +0500

    obj operations

--- HoBager/main.js
+++ HoBager/main.js
@@ -1,16 +1,19 @@
+const operations = {
+    add: '+',
+    multi: '*',
+    substract: '-'
+}
+
 function calc(operation,a,b){
     if(!(+a) || !(+b)){
         return 'не правильно набраны переменные'
     }
 
     switch(operation){
-        case 'add': return (+a) + (+b)
-        case 'multi': return a * b
-        case 'subtract': return a - b
+        case operations.add: return (+a) + (+b)
+        case operations.multi: return a * b
+        case operations.substract: return a - b
         default : return 'недопустимый оператор' 
     }
 }
 
-console.log(calc('add', 6, 2));
-console.log(calc('multi', 4, 2));
-console.log(calc('subtract',10, 2));
