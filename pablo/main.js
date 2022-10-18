0df0e38869c8755822979a54fcbfa0c7b0b58db4
Pavlik1996 <miki2012miki@gmail.com>
2022-10-18 12:57:21 +0300

    calc object

--- pablo/main.js
+++ pablo/main.js
@@ -1,28 +1,24 @@
-// function calc(action, a, b) {
-//   if (action == "add") {
-//     return a + b;
-//   } else if (action == "multi") {
-//     return a * b;
-//   } else {
-//     return a - b;
-//   }
-// }
-
-// console.log(calc("subtract", 3, 2));
-// console.log(calc("multi", 1, 2));
-// console.log(calc("add", 1, 2));
+const operations = {
+  add: `+`,
+  multi: `*`,
+  subtract: `-`,
+};
 
 function calc(action, a, b) {
-  switch (action) {
-    case "add":
-      return a + b;
-    case "multi":
-      return a * b;
-    case "subtract":
-      return a - b;
+  if (Number.isInteger(a, b)) {
+    switch (action) {
+      case operations.add:
+        return console.log(a + b);
+      case operations.multi:
+        return console.log(a * b);
+      case operations.subtract:
+        return console.log(a - b);
+    }
+  } else {
+    return console.log("Не число");
   }
 }
 
-console.log(calc("subtract", 3, 2));
-console.log(calc("multi", 1, 2));
-console.log(calc("add", 1, 2));
+calc(operations.subtract, 3, 2);
+calc(operations.multi, 1, 2);
+calc(operations.add, 1, 2);
