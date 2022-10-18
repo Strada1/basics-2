66e9621b997a9c6728a58a863c502ed32aa238f0
Dimitry Nasrtdinov <75541143+HolloWiccup@users.noreply.github.com>
2022-10-18 21:20:03 +0300

    Update calc.js

--- hollowiccup/calc.js
+++ hollowiccup/calc.js
@@ -1,14 +1,12 @@
-const operations = {
-  add: a + b,
-  multi: a * b,
-  subtract: a - b,
+function calc(operator, a, b) {
+  const operations = {
+    add: a + b,
+    multi: a * b,
+    subtract: a - b,
+  };
+  if (operations[operator] !== undefined) return operations[operator];
 }
 
-function calc(operator, a, b) {
-     if(operations[operator] !== undefined) 
-       return operations[operator];
- }
- 
- console.log(calc("add", 1, 2));
- console.log(calc("multi", 1, 2));
- console.log(calc("subtract", 3, 2));
+console.log(calc("add", 1, 2));
+console.log(calc("multi", 1, 2));
+console.log(calc("subtract", 3, 2));
