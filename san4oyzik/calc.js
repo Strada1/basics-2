0f57e8624ee1e72d4d190e00648623c88e415d6d
San4oyzik <san4oyzik@gmail.com>
2022-10-19 23:13:54 +0300

    17 день клониование объектов

--- san4oyzik/calc.js
+++ san4oyzik/calc.js
@@ -1,31 +1,28 @@
-
-
-function calc(operator, a, b) {
-    const operations = {
-        add: a + b,
-        multi: a * b,
-        sub: a - b,
-    }
-
-    if (operations[operator] !== undefined) {
-        return operations[operator];
-    }
-    
-    // a = Number(a);
-    // b = Number(b);
-    
-
-    // switch(operator) {
-    //     case operations.add:
-    //         return a + b;
-    //     case operations.multi:
-    //         return a * b;
-    //     case operations.sub:
-    //         return a - b;
-    //     // default:
-    //     //     return 'Неизвестная операция';
-    // }
+const operations = {
+    add: '+',
+    multi: '*',
+    sub: '-',
 }
 
-console.log(calc('add', 45, 10));
-console.log(calc('multi', 15, 10));
+function calc(operator, a, b) {
+
+    if (a !== Number(a) || b !== Number(b)) {
+        return "Введите число!";
+    }
+
+    switch(operator) {
+        case operations.add:
+            return a + b;
+        case operations.multi:
+            return a * b;
+        case operations.sub:
+            return a - b;
+        default:
+            return 'Неизвестная операция';
+    }
+}
+
+console.log(calc(operations.add, 14, 10));
+console.log(calc(operations.multi, 15, 10));
+console.log(calc(operations.sub, 15, 10));
+console.log(calc('adsadsa', 15, 10));
--- san4oyzik/main.js
+++ san4oyzik/main.js
@@ -1,11 +1,25 @@
 const prompt = require("prompt-sync")();
 
-let user = {
-    name: 'John',
-    age: 30
-};
+// let user = {
+//     name: 'John',
+//     age: 30
+// };
 
-let key = prompt('Что вы хотите знать о пользователе? ', 'name');
+// let key = prompt('Что вы хотите знать о пользователе? ', 'name');
 
 
-console.log(user[key]);
+// console.log(user[key]);
+
+let salaries = {
+    John: 100,
+    Ann: 160,
+    Pete: 130
+}
+
+
+let sum = 0;
+for (let key in salaries) {
+    sum += salaries[key];
+}
+
+console.log(sum);
