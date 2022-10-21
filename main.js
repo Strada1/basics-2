a3f4ac09a19db751bea44653a3ee4918229d84c2
Vadim Sysoev <sysoev.dev@gmail.com>
2022-10-21 10:45:11 +0300

    feat: add functions

--- main.js
+++ main.js
@@ -3,3 +3,31 @@ const list = {
   "make a bed": "Done",
   "write a post": "To Do",
 }
+
+const DEFAULT_STATUS = "To Do";
+
+function addTask(taskName) {
+
+}
+
+function changeStatus(taskName, taskStatus) {
+  const isValid = list[taskName] != undefined;
+  if (isValid) {
+    list[taskName] = taskStatus;
+  } else {
+    return 'Task not found'
+  }
+}
+
+function deleteTask(taskName) {
+  const isValid = list[taskName] != undefined;
+  if (isValid) {
+    delete list[taskName]
+  } else {
+    return 'Task not found'
+  }
+}
+
+function showList() {
+
+}
