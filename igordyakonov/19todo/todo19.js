d691c688fefc6afbd183a1fd9a405f4adb0a6241
Igor Dyakonov <igordyakonov2019@gmail.com>
2022-10-21 13:51:59 +0300

    add todo

---
+++ igordyakonov/19todo/todo19.js
@@ -0,0 +1,48 @@
+const list = {
+    "create a new practice task": "In Progress",
+    "make a bed": "Done",
+    "write a post": "To Do",
+}
+
+function changeStatus (property, value){
+    list[property] = value;
+}
+
+function addTask (task, value = "To Do"){
+    list[task] = value;
+}
+
+function deleteTask (task){
+    delete list[task];
+}
+
+function showLost (){
+    console.log("\040\040\040Todo:");
+    for (let key in list){
+        if (list[key] === "To Do"){
+            console.log("\t" + key);
+        }
+        
+    }
+    console.log("\040\040\040In Progress:");
+    for (let key in list){
+        if (list[key] === "In Progress"){
+            console.log("\t" + key);
+        }
+    }
+    console.log("\040\040\040Done:");
+    for (let key in list){
+        if (list[key] === "Done"){
+            console.log("\t" + key);
+        }
+    }
+}
+
+
+changeStatus ("create a new practice task", "To Do");
+changeStatus ("make a bed", "To Do");
+changeStatus ("write a post", "In Progress");
+addTask ("have a walk");
+addTask("home work Strada", "Done");
+deleteTask("have a walk");
+showLost();
