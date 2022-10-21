e902a866f6da7093c56e611d0bf858d326c813ff
Denis Butyrskiy <d.butyrskiy@gmail.com>
2022-10-21 07:08:52 +0700

    change st12.js

--- butyrskiy/st12.js
+++ butyrskiy/st12.js
@@ -14,26 +14,3 @@ function calc(operator, a, b) {
 console.log(calc('add', 5, 5));
 console.log(calc('multi', 5, 5));
 console.log(calc('subtract', 5, 5));
-
-// Example #2
-
-function color(color) {
-  switch (color) {
-    case 'Orange':
-      console.log('Color is Orange');
-      break;
-    case 'Red':
-      return console.log('Color is Red');
-    case 'White':
-    case 'Green':
-      return console.log('Color White or Green');
-    default:
-      console.log('Color is not defined');
-  }
-}
-
-color('Orange');
-color('Red');
-color('White');
-color('Green');
-color('Black');
