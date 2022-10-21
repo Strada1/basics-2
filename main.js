231355d376799675606be309d40085c58b1be71a
Vadim Sysoev <sysoev.dev@gmail.com>
2022-10-22 00:37:27 +0300

    feat: Complete task todo

--- main.js
+++ main.js
@@ -4,11 +4,16 @@ const list = {
   "write a post": "To Do",
 }
 
-const DEFAULT_STATUS = "In Progress";
-const TASK_ERROR = "Task not found";
+const TODO_STATUS = {
+  TODO: "To Do",
+  IN_PROGRESS: "In Progress",
+  DONE: "Done",
+  DEFAULT_STATUS: "In Progress",
+  UNKNOW_TASK_STATUS: "Task or status not found",
+}
 
 function addTask(taskName) {
-  list[taskName] = DEFAULT_STATUS;
+  list[taskName] = TODO_STATUS.DEFAULT_STATUS;
 }
 
 function changeStatus(taskName, taskStatus) {
@@ -16,19 +21,47 @@ function changeStatus(taskName, taskStatus) {
   if (isValid) {
     list[taskName] = taskStatus;
   } else {
-    return TASK_ERROR;
+    return TODO_STATUS.UNKNOW_TASK_STATUS;
   }
 }
 
 function deleteTask(taskName) {
   const isValid = taskName in list;
   if (isValid) {
-    delete list[taskName]
+    delete list[taskName];
   } else {
-    return TASK_ERROR;
+    return TODO_STATUS.UNKNOW_TASK_STATUS;
   }
 }
 
 function showList() {
+  let resultTodo = '';
+  let resultInProgress = '';
+  let resultDone = '';
 
+  for (let key in list) {
+    switch (list[key]) {
+      case TODO_STATUS.TODO:
+        resultTodo += `	${key}\n`;
+        break;
+      case TODO_STATUS.IN_PROGRESS:
+        resultInProgress += `	${key}\n`;
+        break;
+      case TODO_STATUS.DONE:
+        resultDone += `	${key}\n`;
+        break;
+      default:
+        break;
+    }
+  }
+  if (resultTodo.length == 0) {
+    resultTodo = "	-";
+  } else if (resultInProgress.length == 0) {
+    resultInProgress = "	-";
+  } else if (resultDone.length == 0) {
+    resultDone = "	-";
+  }
+  console.log(`Todo:\n${resultTodo}`);
+  console.log(`${TODO_STATUS.IN_PROGRESS}:\n${resultInProgress}`);
+  console.log(`${TODO_STATUS.DONE}:\n${resultDone}`);
 }
