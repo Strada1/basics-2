49bb6d6f728cf33ba7c04f7dacf037d7cf97a289
Alexander Pierce <phamarel@gmail.com>
2022-10-22 14:51:49 +0300

    ToDo task

--- sawyer/main.js
+++ sawyer/main.js
@@ -1,32 +1,47 @@
-function calc(op, arg1, arg2) {
-
-    let result;
-
-    if(typeof arg1 == "number" && typeof arg2 == "number")
-    {
-
-        switch(op) {
-            case "add":
-                result = arg1 + arg2;
-                break;
-
-            case "multi":
-                result = arg1 * arg2; 
-                break; 
-
-            case "subtract":
-                result = arg1 - arg2;
-                break;
-
-            default:
-                result = "wrong operator";          
-        } 
-
-    }
-    else
-        result = "wrong argument type";
-
-    return result;
+const list = {
+    "create a new practice task":"In Progress",
+    "make a bed":"Done",
+    "write a post":"To Do",
+    "Walking in park":"To Do"  
 }
 
-console.log(calc("add", 2, 4));
+function changeStatus(Key, Value) {
+    list[Key] = Value;
+}
+
+function addTask(Task) {
+    list[Task] = "To Do";
+}
+
+function deleteTask(Task) {
+    delete list[Task];
+}
+
+function showList() {
+
+    console.log("ToDo:")
+
+    for(key in list)
+    {
+        if(list[key] == "To Do")
+            console.log("\t", key);       
+    }
+
+    console.log("\nIn Progress:")
+
+    for(key in list)
+    {
+        if(list[key] == "In Progress")
+            console.log("\t", key);       
+    }
+
+    console.log("\nDone:")
+
+    for(key in list)
+    {
+        if(list[key] == "Done")
+            console.log("\t", key);       
+    }  
+}
+
+//s
