7fe80208e0e2cfc72a7564e8522427da19013a90
Anton <a.orlov1987@gmail.com>
2022-10-22 19:10:16 +0700

    Added object OPERATIONS

--- antonorlov/main.js
+++ antonorlov/main.js
@@ -1,23 +1,24 @@
-const ADD = 'add'
-const MULTI = 'multi'
-const SUBTRACT = 'subtract'
+const OPERATIONS = {
+  ADD: '+',
+  MULTI: '*',
+  SUBTRACT: '-',
+};
 
-function calc(ident, a, b) {
-    
+function calc(ident, a, b) { 
   if (a !== Number(a) || b !== Number(b)) {
     return 'Вы вводите строку, уважаемый'
   }
 
   switch (ident) {
-    case ADD:
-      return a + b;
-    case MULTI:
+    case OPERATIONS.ADD:
+      return Number(a) + Number(b);
+    case OPERATIONS.MULTI:
       return a * b;
-    case SUBTRACT:
+    case OPERATIONS.SUBTRACT:
       return a - b;
   }
 };
 
-console.log(calc('add', 1, 2));
-console.log(calc('multi', 1, 2));
-console.log(calc('subtract', 3, 2));
+console.log(calc(OPERATIONS.ADD, 1, 2));
+console.log(calc(OPERATIONS.MULTI, 1, 2));
+console.log(calc(OPERATIONS.SUBTRACT, 3, 2));
