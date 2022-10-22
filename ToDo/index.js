3ef174adfd16bd85a6494a9bb7ff0c679887d721
Nastya Shevchenko <anastasias859@gmail.com>
2022-10-22 12:00:46 +0300

    ...

--- ToDo/index.js
+++ ToDo/index.js
@@ -32,33 +32,6 @@ function changeStatus(item, changedStatus) {
     } return errors.error
 }
 
-function showList() {
-    let doneList = '';
-    let inProgressList = '';
-    let toDoList = '';
-    console.log('Done:');
-        for(item in tasks) {
-            if(tasks[item] == 'Done') {
-                console.log(item);
-            }
-        };
-    console.log('In Progress:');
-        for(item in tasks) {
-            if(tasks[item] == 'In Progress') {
-                console.log(item);
-            }   
-        };
-    console.log('To do:');
-        for(item in tasks) {
-            if(tasks[item] == 'To do') {
-                console.log(item);
-            } 
-            
-        };
-
-        
-}
-
 
 addTask('Do the dishes');
 deleteTask('create a new practice task');
