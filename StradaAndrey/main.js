2c8a1a368f07c88adffa98921ae3a4e57cbb4e78
AndreyKubarenko <akubarenko1986@gmail.com>
2022-10-23 09:12:45 +0300

    todo готов

--- StradaAndrey/main.js
+++ StradaAndrey/main.js
@@ -1,21 +1,19 @@
 const status = {
-   TO_DO: 'to do',
-   IN_PROGRESS : 'in progress',
-   DONE : 'done',
+   TO_DO: 'Todo',
+   IN_PROGRESS : 'In progress',
+   DONE : 'Done',
 };
 
 const list = {
-   'To sleep' : 'to do',
+   'To sleep' : 'Todo',
 };
 
 function changeStatus(task, status) {
-   for(let key in list){
-      if(key === task){
-         return list[task] = status;
-      } else {
-         return console.log(`нет такой задачи "${task}"`);
-      }
-   };
+   if(task in list){
+      list[task] = status;
+   } else {
+      console.log(`нет такой задачи "${task}"`);
+   }
 }
 
 function addTask(task){
@@ -36,8 +34,30 @@ function deleteTask(task){
       }
 }
 
-function showStatus() {
+function showList() {
+   let todo = '';
+   let in_progress = '';
+   let done = '';
+   for (let key in list){
+      if (list[key] === status.TO_DO){
+         todo += '\n\t'+ key;
+      } else if(list[key] === status.IN_PROGRESS){
+         in_progress += '\n\t'+ key;
+      } else if(list[key] === status.DONE){
+         done += '\n\t'+ key;
+      }
+   }
 
+   if (todo == false){
+      todo = '\n\t-';
+   } else if(in_progress == false){
+      in_progress = '\n\t-';
+   } else if (done == false) {
+      done = '\n\t-';
+   }
+   console.log(`${status.TO_DO}:${todo}`)
+   console.log(`${status.IN_PROGRESS}:${in_progress}`)
+   console.log(`${status.DONE}:${done}`)
 }
 
 changeStatus('To sleeep', status.DONE);
@@ -45,9 +65,12 @@ addTask('To eat');
 addTask('To eat');
 addTask('To work');
 addTask('To go for a walk');
-console.log(list);
+addTask('To run');
 deleteTask('To eat'); 
+changeStatus('To work', status.DONE);
+changeStatus('To run', status.IN_PROGRESS);
 console.log(list);
+showList()
 
 
 
