c622c2ac1d30a42ddb6b62327f9fea3ec44aa714
zdarovEzhov <ezhov.95@gmail.com>
2022-10-24 22:15:41 +0300

    add showList

--- todo/#19-ToDo.js
+++ todo/#19-ToDo.js
@@ -1,17 +1,32 @@
-const list = {}
+const list = {
+    "make a bed": "Done",
+    "make a beds": "Done",
+    "make a bedf": "Done",
+    "make a bedg": "Done",
+    "make a bedh": "Done",
+}
 
 function addTask(task_name) {
     return list[task_name] = `ToDo`;
 }
 
 function changeStatus(task, status) {
-    return list[task] = status;
+    // return list[task] = status;
 }
 
 function showList(obj) {
-    for (let key in obj) {
-        console.log(key);
+    const sort = {
+        Done: ``,
+
+
+    };
+    for (let objKey in obj) {
+        if (obj[objKey] === `Done`) {
+            sort.Done += `\t` + objKey + ` \n`;
+        }
     }
+    return `Done:\n${sort.Done}`;
+
 }
 
 addTask(`test`);
@@ -19,6 +34,7 @@ addTask(`test2`);
 addTask(`test3`);
 addTask(`test4`);
 console.log(showList(list));
+console.log();
 
 // console.log(changeStatus(`test2`, `Done`));
 // console.log(changeStatus(`test4`, `In progres`));
