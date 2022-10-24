e691906c5849918a548e0e6731b823bb0abbf058
aandrosova <aandrosova@tradingview.com>
2022-10-24 22:52:11 +0300

    add flags

--- evilduck/main.js
+++ evilduck/main.js
@@ -23,25 +23,44 @@ function addTask(task) {
 
 function showList() {
     console.log("TO DO:");
+    let flag = false;
     for (let key in list) {
         if (list[key] === toDo) {
+            flag = true;
             console.log("   ", key)
         }
     }
+    if (!flag) {
+        console.log("-");
+      }
+
     console.log("   ");
     console.log("IN PROGRESS:");
+    let flag2 = false;
     for (let key in list) {
         if (list[key] === inProgress) {
+            flag2 = true;
             console.log("   ", key)
         }
+
     }
+    if (!flag2) {
+        console.log("-");
+      }
+
     console.log("   ");
     console.log("DONE:");
+    let flag3 = false;
     for (let key in list) {
         if (list[key] === done) {
+            flag3 = true;
             console.log("   ", key)
         }
-    } 
+
+    }
+    if (!flag3) {
+        console.log("-");
+      }
 
  }
 
