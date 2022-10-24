a807a246685d69134cf92bc459461b3cdb6b89e1
platonmax <maxcarpenter@gmail.com>
2022-10-24 20:52:57 +0300

    todo ver 1.1

--- PlatonMax/practice-19/todo.js
+++ PlatonMax/practice-19/todo.js
@@ -1,37 +1,69 @@
+const STATUS = {
+	IN_PROGRESS: "In Progress",
+	DONE: "Done",
+	TO_DO: "To Do",
+}
+
+const ERRORS = {
+	TASK_NOT_FOUND: "Такой задачи не найдено",
+	TASK_ALREADY_EXIST: "Такая задача уже существует",
+}
+
 const list = {
-	"create a new practice task": "In Progress",
-	"make a bed": "Done",
-	"write a post": "To Do",
-	changeStatus(nameTask, taskProgress) {
+	"create a new practice task": STATUS.DONE ,
+	"make a bed": STATUS.DONE,
+	"write a post": STATUS.TO_DO,
+	
+	changeStatus(nameTask, taskStatus) {
 		if (this[nameTask])
-			this[nameTask] = taskProgress
+			this[nameTask] = taskStatus
 		else
-			console.log('Такой задачи не найдено [change status')
+			console.log(ERRORS.TASK_NOT_FOUND)
 	},
 
 	addTask(nameTask) {
 		if (!this[nameTask])
 			this[nameTask] = 'new task'
 		else
-			console.log('Задача уже существует')
+			console.log(ERRORS.TASK_ALREADY_EXIST)
 	},
 
 	deleteTask(nameTask) {
 		if (this[nameTask])
 			delete this[nameTask]
 		else
-			console.log('Такой задачи не найдено [delete]')
-
+			console.log(ERRORS.TASK_NOT_FOUND)
 	},
+
+	showList() {
+		let toDoStatus = STATUS.TO_DO;
+		let doneStatus = STATUS.DONE;
+		let inProgressStatus = STATUS.IN_PROGRESS;
+
+		for (let key in list) {
+		 if (list[key] === STATUS.TO_DO)	toDoStatus +=`\n   ${key}`;
+		 if (list[key] === STATUS.DONE) doneStatus +=`\n   ${key}`;
+		 if (list[key] === STATUS.IN_PROGRESS) inProgressStatus +=`\n   ${key}`;	
+		}
+
+		 if (toDoStatus.length === STATUS.TO_DO.length) toDoStatus += '\n   -';
+		 if (doneStatus.length === STATUS.DONE.length) doneStatus += '\n   -';
+		 if (inProgressStatus.length === STATUS.IN_PROGRESS.length) inProgressStatus += '\n   -';
+
+		return `${doneStatus}\n${toDoStatus}\n${inProgressStatus}`;
+	}
+
 }
-// console.log(list);
-// list.changeStatus("write a post1", "Done"); // несуществующая задача
-// console.log(list["write a post"]);
-// list.changeStatus("write a post", "Done"); // существующая задача
+
+list.changeStatus("write a post1", STATUS.DONE); // несуществующая задача
+list.changeStatus("write a post", STATUS.DONE); // существующая задача
 
 list.addTask('have a walk');
-console.log(list);
-console.log(list['have a walk']);
+list.changeStatus('have a walk', STATUS.TO_DO);
+
+console.log(list.showList());
+
 list.deleteTask('have a walk');
-console.log(list);
-list.deleteTask('have a walk');
+list.changeStatus("write a post", STATUS.IN_PROGRESS);
+
+console.log(list.showList());
