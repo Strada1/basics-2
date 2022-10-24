48b49b706ba44c370b50f81637e2b5e47c9ba3a2
Vladimir <wizart88@gmail.com>
2022-10-24 22:03:19 +0400

    feat: add check if for zero tasks

--- good-speedf1/19day-to-do-list.js
+++ good-speedf1/19day-to-do-list.js
@@ -1,3 +1,9 @@
+const STATUS = {
+	TO_DO: 'To Do',
+	DONE: 'Done',
+	IN_PROGRESS: 'In Progress',
+}
+
 // список задач
 const list = {
 	"create a new practice task": "In Progress",
@@ -9,10 +15,10 @@ const list = {
 function changeStatus(list, task, status) {
 	if (!(task in list)) {
 		return "task not found"
-	} else if (!(status == "In Progress" || status == "Done" || status == "To Do")) {
-		return "please write the correct status"
+	} else if (!(status == STATUS.IN_PROGRESS || status == STATUS.DONE || status == STATUS.TO_DO)) {
+		console.log("please write the correct status"); 
 	} else {
-		return list[task] = status
+		list[task] = status
 	}
 }
 // changeStatus(list, "write a post", Done)
@@ -20,9 +26,9 @@ function changeStatus(list, task, status) {
 // добавление задачи
 function addTask(list, task) {
 	if ((task in list)) {
-		return "this task is already in list? add new task"
+		console.log("this task is already in list? add new task"); 
 	} else {
-		return list[task] = "To Do"
+		list[task] = STATUS.TO_DO
 	}
 }
 // addTask(list, "kiss them")
@@ -30,37 +36,62 @@ function addTask(list, task) {
 // удаление задачи
 function deleteTask(list, task) {
 	if (!(task in list)) {
-		return "task not exist at list"
+		console.log("task not exist at list"); 
 	} else {
-		return delete list[task]
+		delete list[task]
 	}
 }
 // deleteTask(list, "write a post")
 
 // вывод списка задач
 function showList (list) {
+	let count_in_progress = 0,
+			count_to_do = 0,
+			count_done = 0;
 	console.log("In Progress");
 	for (let task in list) {
-		if (list[task] === "In Progress") {
+		if (list[task] === STATUS.IN_PROGRESS) {
+			count_in_progress++;
 			console.log(task + '\n');
 		}
 	}
+	if (count_in_progress === 0) {
+		console.log(' - ');
+	}
+
+	count = 0;
 	console.log("Done");
 	for (let task in list) {
-		if (list[task] === "Done") {
+		if (list[task] === STATUS.DONE) {
+			count_done++;
 			console.log(task + '\n');
 		}
 	}
+	if (count_done === 0) {
+		console.log(' - ');
+	}
+
+	count = 0;
 	console.log("To Do");
 	for (let task in list) {
-		if (list[task] === "To Do") {
+		
+		if (list[task] === STATUS.TO_DO) {
+			count_to_do++;
 			console.log(task + '\n');
 		}
 	}
+	if (count_to_do === 0) {
+		console.log(' - ');
+	}
+	// console.log('\n');
 }
 showList(list)
 addTask(list, "kiss them")
 addTask(list, "kill them")
 changeStatus(list, "write a post", "Done")
 deleteTask(list, "kill them")
+// deleteTask(list, "make a bed")
+// deleteTask(list, "write a post")
+// deleteTask(list, "create a new practice task")
+// deleteTask(list, "kiss them")
 showList(list)
