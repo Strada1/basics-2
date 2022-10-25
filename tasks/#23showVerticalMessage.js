29084d6a051d62690815321ad32b9dff2d76caa8
Pavlik1996 <miki2012miki@gmail.com>
2022-10-25 15:57:34 +0300

    23

---
+++ tasks/#23showVerticalMessage.js
@@ -0,0 +1,14 @@
+function showVerticalMessage(value) {
+  if (value[0] === "s") {
+    value = "S" + value.slice(1, 7);
+    for (let char of value) {
+      console.log(char);
+    }
+  } else {
+    for (let char of value.slice(0, 7)) {
+      console.log(char);
+    }
+  }
+}
+
+showVerticalMessage("strada");
