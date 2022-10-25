0dc4f69a8915c4d52dfa294920f54d128b0a25b8
Pavlik1996 <miki2012miki@gmail.com>
2022-10-25 17:38:46 +0300

    edit #2

--- tasks/#23showVerticalMessage.js
+++ tasks/#23showVerticalMessage.js
@@ -1,13 +1,9 @@
 function showVerticalMessage(value) {
   if (value[0] === "s") {
     value = "S" + value.slice(1, 7);
-    for (let char of value) {
-      console.log(char);
-    }
-  } else {
-    for (let char of value.slice(0, 7)) {
-      console.log(char);
-    }
+  }
+  for (let char of value.slice(0, 7)) {
+    console.log(char);
   }
 }
 
