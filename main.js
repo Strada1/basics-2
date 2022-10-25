c566a61987d7a758fbcd5699be968586e1d4915f
Vadim Sysoev <sysoev.dev@gmail.com>
2022-10-25 14:44:03 +0300

    feat: Add function showVerticalMessage and test

--- main.js
+++ main.js
@@ -1,3 +1,24 @@
 function showVerticalMessage(str) {
+  let resultStr = '';
+  let verticalStr = '';
 
+  if (str[0] == 's') {
+    resultStr = 'S' + str.slice(1)
+  } else {
+    resultStr = str;
+  }
+
+  if (resultStr.length > 7) {
+    resultStr = resultStr.slice(0, 7);
+  }
+
+  for (let char of resultStr) {
+    verticalStr += char + '\n';
+  }
+
+  console.log(verticalStr);
 }
+
+showVerticalMessage('strada');
+showVerticalMessage('stradaaaaaaaaaaaaaaaaaaaaa');
+showVerticalMessage('testtesttest');
