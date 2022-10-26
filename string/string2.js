4df103ff551a5813c1c56a113a812caeb5ae1f72
mikekoman <koman.mike@yahoo.com>
2022-10-26 11:47:05 +0300

    deleted else if

--- string/string2.js
+++ string/string2.js
@@ -4,10 +4,11 @@ function showVerticalMessage(str) {
   let char = "s";
 
   if (str[0] === char) {
-    str = str[0].toUpperCase() + str.slice(1);
-  } else if (str.length > 7) {
+    str = str[0].toUpperCase() + str.slice(1, 7);
+  } else {
     str = str.slice(0, 7);
   }
+
   for (let i of str) {
     str2 += i + "\n";
   }
