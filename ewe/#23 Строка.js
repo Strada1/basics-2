752eaacdcd7d04a38dba6ea9e92ea7da100a6390
Togri <aleksandr.shuvakin@gmail.com>
2022-10-26 20:26:54 +0500

    строка, но не вертикальная

--- ewe/#23 Строка.js
+++ ewe/#23 Строка.js
@@ -1,21 +1,45 @@
 function showVerticalMessage(inputValueVMessage) {
+    let errorInput = inputValueVMessage;
+    const ERROR_MESSAGE = "Совершена ошибка при вводе, введите другое слово.";
+    
+    if (typeof(errorInput) != "string") {
+        return console.log(ERROR_MESSAGE);
+    } else if (errorInput.trim() == '') {
+        return console.log(ERROR_MESSAGE);
+    } else if (errorInput === '0') {
+        return console.log(ERROR_MESSAGE);
+
+    }
+   
     const propertyValue = {
         BIG_FIRST: inputValueVMessage[0].toUpperCase(),
         LOW_FIRST: inputValueVMessage[0].toLowerCase(),
         STANDART_FIRST: inputValueVMessage[0],
-        SLICE_TEXT: inputValueVMessage.slice(0, 7)
+        LENGTH_TEXT: inputValueVMessage.lenght,
+        SLICE_TEXT_END: inputValueVMessage.slice(1, 7),
+        SLICE_TEXT_FULL: inputValueVMessage.slice(0, 7),
+        RETURN_BACK: inputValueVMessage,
     };
 
+    if (propertyValue.LENGTH_TEXT > 7) {
+        inputValueVMessage = propertyValue.BIG_FIRST + propertyValue.SLICE_TEXT_END;
+        } else if (propertyValue.LENGTH_TEXT === 0) {
+        console.log(propertyValue.ERROR_MESSAGE);
+    } else {
+        inputValueVMessage = propertyValue.BIG_FIRST + propertyValue.SLICE_TEXT_END;
+    }
+    console.log(inputValueVMessage);
+
  
-};
+}
 
 
 showVerticalMessage('Stradaite');
 showVerticalMessage('strada');
 showVerticalMessage('Dantistsr23');
-// showVerticalMessage('0');
-// showVerticalMessage('');
-// showVerticalMessage(' ');
-// showVerticalMessage(12345678);
-// showVerticalMessage();
+showVerticalMessage('0');
+showVerticalMessage('');
+showVerticalMessage(' ');
+showVerticalMessage(12345678);
+showVerticalMessage();
 // Первая мысль. Циклом выводить каждую букву. Цикл проверяет наличие и выводит.
