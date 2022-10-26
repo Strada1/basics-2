f1c9e30cdb69fd94e4a7f060f18d6a8c84f78ebb
Pavlik1996 <miki2012miki@gmail.com>
2022-10-26 10:50:08 +0300

    num 23 DONE

--- tasks/#23showVerticalMessage.js
+++ tasks/#23showVerticalMessage.js
@@ -1,10 +1,12 @@
 function showVerticalMessage(value) {
+  let str = "";
   if (value[0] === "s") {
     value = "S" + value.slice(1, 7);
   }
   for (let char of value.slice(0, 7)) {
-    console.log(char);
+    str += char + "\n";
   }
+  console.log(str);
 }
 
 showVerticalMessage("strada");
