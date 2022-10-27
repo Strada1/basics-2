6b37e52f480037dc1534676c525b4404fd06a0b2
wayleet <wayleet@yandex.ru>
2022-10-27 23:28:44 +0300

    работа над ошибками

--- kostyawayleet/todo.js
+++ kostyawayleet/todo.js
@@ -1,70 +1,76 @@
+const STATUS = {
+   TODO: "To Do",
+   DONE: "Done",
+   INPROGRESS: "In Progress",
+};
+
 const list = {
-	"write a post": "To Do",
-	"make a bed": "Done",
-	"create a new practice task": "In Progress",
+   "write a post": "To Do",
+   "make a bed": "Done",
+   "create a new practice task": "In Progress",
 };
 
 function changeStatus(task, status) {
-	let flag = false;
-	if (task in list) {
-		list[task] = status;
-		flag = true;
-	}
-	return flag;
+   let flag = false;
+   if (task in list) {
+      list[task] = status;
+      flag = true;
+   }
+   return flag;
 }
 
 function addTask(newTask) {
-	let flag = false;
-	if (!(newTask in list)) {
-		list[newTask] = "To Do"
-		flag = true;
-	}
-	return flag;
+   let flag = false;
+   if (!(newTask in list)) {
+      list[newTask] = STATUS.TODO;
+      flag = true;
+   }
+   return flag;
 }
 
 function deleteTask(task) {
-	let flag = false;
-	if (task in list) {
-		delete list[task];
-		flag = true;
-	}
-	return flag;
+   let flag = false;
+   if (task in list) {
+      delete list[task];
+      flag = true;
+   }
+   return flag;
 }
 
 function showList() {
-	console.log(`ToDo:`)
-	let countToDo = 0;
-	for (let task in list) {
-		if (list[task] === "To Do") {
-			console.log(`\t${task}`);
-			countToDo++;
-		}
-	}
-	if (countToDo == 0) {
-		console.log('-');
-	}
-	console.log(`In Progress:`)
-	let countProgress = 0;
-	for (let task in list) {
-		if (list[task] === "In Progress") {
-			console.log(`\t${task}`);
-			countProgress++;
-		}
-	}
-	if (countProgress == 0) {
-		console.log('-');
-	}
-	console.log(`Done:`)
-	let countDone = 0;
-	for (let task in list) {
-		if (list[task] === "Done") {
-			console.log(`\t${task}`);
-			countDone++;
-		}
-	}
-	if (countDone == 0) {
-		console.log('-');
-	}
+   console.log(STATUS.TODO)
+   let flagToDo = false;
+   for (let task in list) {
+      if (list[task] === STATUS.TODO) {
+         console.log(`\t${task}`);
+         flagToDo = true;
+      }
+   }
+   if (flagToDo == false) {
+      console.log('-');
+   }
+   console.log(STATUS.INPROGRESS)
+   let flagProgress = false;
+   for (let task in list) {
+      if (list[task] === STATUS.INPROGRESS) {
+         console.log(`\t${task}`);
+         flagProgress = true;
+      }
+   }
+   if (flagProgress == false) {
+      console.log('-');
+   }
+   console.log(STATUS.DONE)
+   let flagDone = false;
+   for (let task in list) {
+      if (list[task] === STATUS.DONE) {
+         console.log(`\t${task}`);
+         flagDone = true;
+      }
+   }
+   if (flagDone == false) {
+      console.log('-');
+   }
 }
 
 addTask("buy an elephant");
