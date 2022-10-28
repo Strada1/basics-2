ec05428754e3173c5b33cfb01a7ede84e465ce7a
HolloWiccup <75541143+HolloWiccup@users.noreply.github.com>
2022-10-28 15:30:01 +0300

    todo-do-do

--- todo/todo.js
+++ todo/todo.js
@@ -13,8 +13,55 @@ const list = [ { name: 'create a post', status: "In progress", priority: 'low'  
                { name: 'test2', status: "To Do", priority: 'high'  },
                { name: 'test3', status: "Done", priority: 'high'  },
                { name: 'test4', status: "Done", priority: 'high'  },];
-const isTask = (task) =>
-    list.reduce((result, current) => result || current.name === task, false);
+
+const addTask = (todoList, task) =>{
+    if(!isTask(todoList, task)){
+        todoList.push({
+            name: task,
+            status: STATUS.TODO,
+            priority: PRIORITY.LOW,
+            });
+    }
+}
+
+const changeStatus = (todoList, task, status) => {
+    if(isTask(todoList, task) && isStatus(status)){
+        todoList.forEach(item => {
+            if(item.name === task) item.status = status;
+        });
+    }
+}
+
+const changePriority = (todoList, task, priority) => {
+    if(isTask(todoList, task) && isPriority(priority)){
+        todoList.map(item => {
+            if(item.name === task) item.priority = priority;
+        });
+    }
+}
+
+const deleteTask = (todoList, task) =>{
+    if(isTask(todoList, task)){
+        for(let i = todoList.length - 1; i > 0; i--){
+            if(todoList[i].name === task) todoList.splice(i, 1);
+        }
+    } 
+}
+
+const showList = (todoList) =>{
+    let resultString = '';
+    for(let status in STATUS){
+        resultString += `\n${status}:`;
+        resultString += todoList.reduce((sum, current) => sum +
+            (current.status === STATUS[status] ? 
+            `\n\t"${current.name}",` : ''),'');
+        if(resultString[resultString.length-1] === ':') resultString += `\n\t${EMPTY}`;
+    }
+    console.log(resultString.trim());
+}
+
+const isTask = (todoList, task) =>
+    todoList.reduce((result, current) => result || current.name === task, false);
 
 const isStatus = (status) =>{
     let result = false;
@@ -32,65 +79,8 @@ const isPriority = (priority) =>{
     return result;
 }
 
-const addTask = (task) =>{
-    if(!isTask(task)){
-        list.push({
-            name: task,
-            status: STATUS.TODO,
-            priority: PRIORITY.LOW,
-            });
-    }
-}
-
-const changeStatus = (task, status) => {
-    if(isTask(task) && isStatus(status)){
-        list.forEach(item => {
-            if(item.name === task) item.status = status;
-        });
-    }
-}
-
-const changePriority = (task, priority) => {
-    if(isTask(task) && isPriority(priority)){
-        list.map(item => {
-            if(item.name === task) item.priority = priority;
-        });
-    }
-}
-
-const deleteTask = (task) =>{
-    if(isTask){
-        for(let i = list.length - 1; i > 0; i--){
-            if(list[i].name === task) list.splice(i, 1);
-        }
-    } 
-}
-
-const showList = (todoList) =>{
-    const statusList = createArray();
-    pushArray(statusList, todoList);
-    for(let status in STATUS){
-        console.log(`${STATUS[status]}:\n\t${statusList[STATUS[status]].length === 0 ? 
-            EMPTY : statusList[STATUS[status]].join('\n\t')}`);
-    }
-}
-
-const createArray = () =>{
-    let result = {};
-    for(let key in STATUS){
-        result[STATUS[key]] = [];
-    }
-    return result;
-}
-
-const pushArray = (arr, todoList) => {
-       todoList.forEach(item => {
-           arr[item.status].push(`"${item.name}",`);
-       });
-}
-                        
-addTask('create dsa post');
-changeStatus('test', STATUS.TODO);
-changePriority('create a post', PRIORITY.HIGH);
-deleteTask('create dsa post');
+addTask(list, 'create dsa post');
+changeStatus(list, 'test', STATUS.TODO);
+changePriority(list, 'create a post', PRIORITY.HIGH);
+deleteTask(list, 'create dsa post');
 showList(list);
