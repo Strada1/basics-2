e71ab3683c7dcbd6abfe73663abb6b13c9de3877
Vadim Sysoev <sysoev.dev@gmail.com>
2022-10-28 23:12:53 +0300

    feat: Add function showList

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const list = [{ name: 'create a post', status: 'In progress', priority: 'low' }, { name: 'test', status: 'Done', priority: 'high' }];
+const list = [{ name: 'Comeback', status: 'Done', priority: 'high' }, { name: 'create a post', status: 'In Progress', priority: 'low' }, { name: 'test', status: 'Done', priority: 'high' }];
 
 const TODO_STATUS = {
   TODO: "To Do",
@@ -28,8 +28,9 @@ function addTask(taskName, taskPriority) {
 }
 
 function changeStatus(taskName, taskStatus) {
+  isValid = taskStatus == TODO_STATUS.TODO || taskStatus == TODO_STATUS.IN_PROGRESS || taskStatus == TODO_STATUS.DONE;
   list.find(item => {
-    if (item.name == taskName) {
+    if (item.name == taskName && isValid) {
       item.status = taskStatus;
     }
   });
@@ -45,12 +46,49 @@ function changePriority(taskName, taskPriority) {
 }
 
 function deleteTask(taskName) {
-  list.find((item, index) => {
+  let taskIndex;
+  list.findIndex(function (item, index, array) {
     if (item.name == taskName) {
-      list.splice(index, 1);
+      taskIndex = index;
     }
-  });
+  })
+  list.splice(taskIndex, 1)
 }
 
 
-function showList() { }
+function showList() {
+  let strTodo = `Todo:\n`;
+  let strInProgress = `In Progress:\n`;
+  let strDone = `Done:\n`;
+
+  list.forEach(function (item) {
+    switch (item.status) {
+      case TODO_STATUS.TODO:
+        strTodo += `  ${item.name}\n`;
+        break;
+      case TODO_STATUS.IN_PROGRESS:
+        strInProgress += `  ${item.name}\n`;
+        break;
+      case TODO_STATUS.DONE:
+        strDone += `  ${item.name}\n`;
+        break;
+    };
+  });
+
+  if (strTodo.length == 6) {
+    strTodo += `  -\n`;
+  } else if (strInProgress.length == 6) {
+    strInProgress += `  -\n`;
+  } else if (strDone.length == 6) {
+    strDone += `  -\n`;
+  }
+  console.log(strTodo);
+  console.log(strInProgress);
+  console.log(strDone);
+}
+
+addTask('sleep', TODO_PRIORITY.LOW);
+changeStatus('sleep', TODO_STATUS.DONE);
+changePriority('sleep', TODO_PRIORITY.HIGHT);
+deleteTask('sleep')
+showList();
