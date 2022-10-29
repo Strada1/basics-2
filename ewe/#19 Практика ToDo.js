e14530623889fe1c2badca9708b3745bf75fbcba
Togri <aleksandr.shuvakin@gmail.com>
2022-10-29 15:32:09 +0500

    перестановки

--- ewe/#19 Практика ToDo.js
+++ ewe/#19 Практика ToDo.js
@@ -1,25 +1,37 @@
-// 19 Практика ToDo
-
 const list = {
-  "create a new practice task": "In Progress",
-  "make a bed": "Done",
-  "write a post": "To Do",
+	"create a new practice task": "In Progress",
+	"make a bed": "Done",
+	"write a post": "To Do",
+};
+const errorLog = {
+  messageUndefined: 'Ошибка в вводе задачи или такая задача отсутствует',
+  messageDefined: 'Такая задача уже существует',
+  messageNofined: 'Ошибка в статусе задачи',
 };
 
-function changeStatus(paramTaskName, progressTaskStatus) {
-  if (list[paramTaskName] !== undefined) {
-    list[paramTaskName] = progressTaskStatus;
-  } else {
-    console.log('Невозможно изменить не существующую задачу');
+function changeStatus(param1, param2) {
+  if (list[param1] === undefined) {
+    return console.log(errorLog.messageUndefined);
+  } else { 
+    list[param1] = param2;
   }
 }
 
-function addTask(paramTaskAdd) {
-  list[paramTaskAdd] = "To Do";
+function addTask(params) {
+  if (list[params] === undefined) {
+    list[params] = "To Do";
+  } else {
+    return console.log(errorLog.messageDefined);
+  }
 }
 
-function deleteTask(paramTaskDeleted) {
-  delete list[paramTaskDeleted];
+
+function deleteTask(params) {
+  if (list[params] === undefined) {
+    return console.log(errorLog.messageUndefined);
+  } else {
+    delete list[params];
+  }
 }
 
 function showList() {
@@ -45,7 +57,8 @@ function showList() {
   for(let key in list)
   {
       if(list[key] == "Done")
-          console.log("\t", key);       
+          console.log("\t", key);
+
   }  
   
 }
@@ -54,6 +67,5 @@ function showList() {
 changeStatus("write a post", "Done");
 addTask('have a walk');
 deleteTask('have a walk');
-addTask('run out JS');
 
 showList();
