24842d82ab94fa023aab37878581dcb8c72c76f1
Anton <a.orlov1987@gmail.com>
2022-10-30 01:06:53 +0700

    toDo refactoring

--- antonorlov/main.js
+++ antonorlov/main.js
@@ -1,66 +1,79 @@
 const STATUS = {
-    TODO: 'ToDo',
-    INPROGRESS: 'In Progress',
-    DONE: 'Done',
-};
+	TO_DO: 'toDo',
+	IN_PROGRESS: 'inProgress',
+	DONE: 'done',
+}
 
 const list = {};
-let toDoStat = {};
-let inProgressStat = {};
-let doneStat = {};
-
 
 function addTask(task) {
-    list[task] = STATUS.TODO
-};
-    
+	if (task in list) {
+		console.log('Такая задача уже есть в листе');
+	} else {
+		list[task] = STATUS.TO_DO
+		console.log('Задача успешно добавлена');
+	}
+}
+
 function changeStatus(task, status) {
-    list[task] = status
-};
+	if (task in list) {
+		list[task] = status
+		console.log('Статус успешно изменен');
+	} else {
+		console.log('Нет такой задачи в листе');
+	}
+}
 
-function deleteTask(delTask) {
-    delete list[delTask]
-};
-
-// Add task
-addTask("create a new practice task");
-addTask("make a bed");
-addTask("write a post");
-
-// Change status
-changeStatus("write a post", STATUS.INPROGRESS);
-
-
-for (let key in list) {
-
-    if (list[key] === STATUS.TODO) {
-        toDoStat[key] = '';
-    } else if (list[key] === STATUS.INPROGRESS) {
-        inProgressStat[key] = '';
-    } else if (list[key] === STATUS.DONE) {
-        doneStat[key] = '';
-    }
-};
-
+function deleteTask(task) {
+	if (task in list) {
+		delete list[task]
+		console.log('Задача успешно удалена');
+	} else {
+		console.log('Нет такой задачи для удаления');
+	}
+}
 
 function showList() {
-    console.log ('\n' + 'Todo:')
+	let toDoList = {};
+	let inProgressList = {};
+	let doneList = {};
 
-    for (let key in toDoStat) {
-        console.log('\t' + '"' + key + '"')
-    }
+	for (let key in list) {
+		if(list[key] === STATUS.TO_DO) {
+			toDoList[key] = ''
+		} else if (list[key] === STATUS.IN_PROGRESS) {
+			inProgressList[key] = ''
+		} else if (list[key] === STATUS.DONE) {
+			doneList[key] = ''
+		} 
+	}
 
-    console.log('\n' + 'In Progress:')
+	console.log('\n' + 'Todo:')
+	for (let key in toDoList) {
+		console.log(`\t"${key}",`)
+	}
 
-    for (let key in inProgressStat) {
-        console.log('\t' + '"' + key + '"')
-    }
-    
-    console.log('\n' + 'Done:')
+	console.log('\n' + 'In Progress:')
+	for (let key in inProgressList) {
+		console.log(`\t"${key}"`)
+	}
+	
+	console.log('\n' + 'Done:')
+	for (let key in doneList) {
+		console.log(`\t"${key}"`)
+	}
+}
 
-    for (let key in doneStat) {
-        console.log('\t' + '"' + key + '"')
-    }
-};
+// Added task
+addTask("creat a new practice task");
+addTask("make a bed");
+addTask("write a post");
+addTask("delete task");
+
+//Change status
+changeStatus("write a post", STATUS.IN_PROGRESS);
+
+// Delete task
+deleteTask("delete task");
 
 showList()
