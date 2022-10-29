63630e435ecc5d05f6d8421f31132892611bd2ae
xeleron <xelseo@gmail.com>
2022-10-29 11:24:38 +0300

    added todo with array

--- xeleron88/toDoArr.js
+++ xeleron88/toDoArr.js
@@ -1,7 +1,7 @@
 const STATUS = {
-  DONE: "done",
-  IN_PROGRESS: "in progress",
   TODO: "to do",
+  IN_PROGRESS: "In progress",
+  DONE: "done",
 };
 
 const PROIORITY = {
@@ -12,6 +12,8 @@ const PROIORITY = {
 const list = [
   { name: "create a post", status: "In progress", priority: "low" },
   { name: "test", status: "done", priority: "high" },
+  { name: "test3", status: "In progress", priority: "high" },
+  { name: "test5", status: "In progress", priority: "high" },
 ];
 
 function changeStatus(
@@ -44,8 +46,23 @@ function deleteTask(todolist, key) {
   todolist.splice(indexKey, 1);
 }
 
+function showList(todolist) {
+  for (const key in STATUS) {
+    let result = [];
+    console.log(STATUS[key].toUpperCase());
+    todolist.map((item) => {
+      if (item.status === STATUS[key]) {
+        result.push(`\t${item.name}`);
+      }
+    });
+    result = result.length === 0 ? `\t-` : result.join('\n');
+    console.log(`${result}\n`);
+  }
+}
 
-
-addTask(list, "test2", "done", "low");
+addTask(list, "test7", STATUS.DONE, PROIORITY.HIGH);
+addTask(list, "test8", STATUS.DONE, PROIORITY.HIGH);
+addTask(list, "test9");
 deleteTask(list, "test");
-console.log(list);
+changeStatus(list, "test7", STATUS.TODO)
+showList(list);
