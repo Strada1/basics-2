37ef75105c770fbfe8717e769855b48c3a879565
HolloWiccup <75541143+HolloWiccup@users.noreply.github.com>
2022-10-29 08:38:53 +0300

    last todo

--- todo/todo.js
+++ todo/todo.js
@@ -4,15 +4,16 @@ const STATUS = {
     DONE: "Done",
     IN_PROGRESS: "In progress",
 }
+
 const PRIORITY = {
     LOW: 'low',
     HIGH: 'high',
 }
+
 const list = [ { name: 'create a post', status: "In progress", priority: 'low'  }, 
                { name: 'test', status: "Done", priority: 'high'  },
                { name: 'test2', status: "To Do", priority: 'high'  },
-               { name: 'test3', status: "Done", priority: 'high'  },
-               { name: 'test4', status: "Done", priority: 'high'  },];
+               { name: 'test3', status: "Done", priority: 'high'  },];
 
 const addTask = (todoList, task) =>{
     if(!isTask(todoList, task)){
@@ -26,58 +27,49 @@ const addTask = (todoList, task) =>{
 
 const changeStatus = (todoList, task, status) => {
     if(isTask(todoList, task) && isStatus(status)){
-        todoList.forEach(item => {
-            if(item.name === task) item.status = status;
-        });
+        let index = findTask(todoList, task);
+        todoList[index].status = status;
     }
 }
 
 const changePriority = (todoList, task, priority) => {
     if(isTask(todoList, task) && isPriority(priority)){
-        todoList.map(item => {
-            if(item.name === task) item.priority = priority;
-        });
+        let index = findTask(todoList, task);
+        todoList[index].priority = priority;
     }
 }
 
 const deleteTask = (todoList, task) =>{
     if(isTask(todoList, task)){
-        for(let i = todoList.length - 1; i > 0; i--){
-            if(todoList[i].name === task) todoList.splice(i, 1);
-        }
+        let index = findTask(todoList, task);
+        todoList.splice(index, 1);
     } 
 }
 
 const showList = (todoList) =>{
     let resultString = '';
     for(let status in STATUS){
-        resultString += `\n${status}:`;
+        resultString += `\n${STATUS[status]}:`;
         resultString += todoList.reduce((sum, current) => sum +
             (current.status === STATUS[status] ? 
             `\n\t"${current.name}",` : ''),'');
-        if(resultString[resultString.length-1] === ':') resultString += `\n\t${EMPTY}`;
+        if(resultString[resultString.length-1] === ':') 
+            resultString += `\n\t${EMPTY}`;
     }
     console.log(resultString.trim());
 }
 
 const isTask = (todoList, task) =>
-    todoList.reduce((result, current) => result || current.name === task, false);
+    todoList.some(item => item.name === task);
 
-const isStatus = (status) =>{
-    let result = false;
-    for(key in STATUS)
-        if(STATUS[key] === status)
-            result = true;
-    return result;
-}
+const isStatus = (status) =>
+    Object.values(STATUS).includes(status);
 
-const isPriority = (priority) =>{
-    let result = false;
-    for(key in PRIORITY)
-        if(PRIORITY[key] === priority)
-            result = true;
-    return result;
-}
+const isPriority = (priority) =>
+    Object.values(PRIORITY).includes(priority);
+
+const findTask = (todoList, task) =>
+    todoList.findIndex(item => item.name === task);
 
 addTask(list, 'create dsa post');
 changeStatus(list, 'test', STATUS.TODO);
