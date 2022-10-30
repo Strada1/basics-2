d5be2aafc0affa5947e3f15de2840471be22daa0
togri <aleksandr.shuvakin@gmail.com>
2022-10-30 23:06:30 +0500

    кажется нащупал

--- ewe/#19 Практика ToDo.js
+++ ewe/#19 Практика ToDo.js
@@ -15,6 +15,10 @@ const sfasf = {
 	"write a post": "To Do",
 };
 
+// let resultOne = list.find(item => item.name === 'test');
+
+// console.log(resultOne);
+
 
 function changeStatus(param1, param2) {
 
@@ -26,6 +30,7 @@ function changePriority(param1, param3) {
 }
 
 function addTask(param1, param2, param3) {
+  list.push({name: param1, status: param2, priority: param3})
 
 }
 
@@ -34,79 +39,84 @@ function deleteTask(param1) {
 
 }
 
-function showList()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-function changeStatus(param1, param2) {
-  if (list[param1] === undefined) {
-    return console.log(errorLog.messageUndefined);
-  } else { 
-    list[param1] = param2;
-  }
-}
-
-function addTask(params) {
-  if (list[params] === undefined) {
-    list[params] = "To Do";
-  } else {
-    return console.log(errorLog.messageDefined);
-  }
-}
-
-
-function deleteTask(params) {
-  if (list[params] === undefined) {
-    return console.log(errorLog.messageUndefined);
-  } else {
-    delete list[params];
-  }
-}
-
 function showList() {
 
-  console.log("ToDo:");
-
-  for(let key in list)
-  {
-      if(list[key] == "To Do")
-          console.log("\t", key);       
-  }
-
-  console.log("\nIn Progress:");
-
-  for(let key in list)
-  {
-      if(list[key] == "In Progress")
-          console.log("\t", key);       
-  }
-
-  console.log("\nDone:");
-
-  for(let key in list)
-  {
-      if(list[key] == "Done")
-          console.log("\t", key);
-
-  }  
-  
 }
 
+console.log(list);
 
-changeStatus("write a post", "Done");
-addTask('have a walk');
-deleteTask('have a walk');
+addTask('Ghbdtn', 'afssfqw', 'fwefe');
 
-showList();
+
+console.log(list);
+
+
+
+
+
+
+
+
+
+
+
+// function changeStatus(param1, param2) {
+//   if (list[param1] === undefined) {
+//     return console.log(errorLog.messageUndefined);
+//   } else { 
+//     list[param1] = param2;
+//   }
+// }
+
+// function addTask(params) {
+//   if (list[params] === undefined) {
+//     list[params] = "To Do";
+//   } else {
+//     return console.log(errorLog.messageDefined);
+//   }
+// }
+
+
+// function deleteTask(params) {
+//   if (list[params] === undefined) {
+//     return console.log(errorLog.messageUndefined);
+//   } else {
+//     delete list[params];
+//   }
+// }
+
+// function showList() {
+
+//   console.log("ToDo:");
+
+//   for(let key in list)
+//   {
+//       if(list[key] == "To Do")
+//           console.log("\t", key);       
+//   }
+
+//   console.log("\nIn Progress:");
+
+//   for(let key in list)
+//   {
+//       if(list[key] == "In Progress")
+//           console.log("\t", key);       
+//   }
+
+//   console.log("\nDone:");
+
+//   for(let key in list)
+//   {
+//       if(list[key] == "Done")
+//           console.log("\t", key);
+
+//   }  
+  
+// }
+
+
+// changeStatus("write a post", "Done");
+// addTask('have a walk');
+// deleteTask('have a walk');
+
+// showList();
