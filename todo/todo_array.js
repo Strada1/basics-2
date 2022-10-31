8b9a7bfa8c84935024d210e3c28baffe5bd704f4
mikekoman <koman.mike@yahoo.com>
2022-10-31 20:46:38 +0300

    changed functions with filter and find methods

--- todo/todo_array.js
+++ todo/todo_array.js
@@ -8,77 +8,92 @@ const PRIORITY = {
   HIGH: "High",
   LOW: "Low",
 };
+const TASK_EXISTS = "already exists, please delete it first.";
 
 const DASH = "-";
 
-const list = [
+let list = [
   { name: "create a post", status: "In Progress", priority: "low" },
   { name: "write a post", status: "Done", priority: "high" },
   { name: "drink water", status: "To Do", priority: "low" },
 ];
 
-function changeStatus(taskName, STATUS, PRIORITY) {
-  let index = list.findIndex((task) => task.name === taskName);
-  list[index].status = STATUS;
-  list[index].priority = PRIORITY;
+function changeStatus(taskName, STATUS) {
+  let task = list.find((task) => task.name === taskName);
+  task.status = STATUS;
+}
+
+function changePriority(taskName, PRIORITY) {
+  let task = list.find((task) => task.name === taskName);
+  task.priority = PRIORITY;
 }
 
 function addTask(taskName) {
-  list.push({ name: taskName, status: STATUS.TODO, priority: PRIORITY.LOW });
+  let task = list.find((task) => task.name === taskName);
+  if (task) {
+    console.log(`"${taskName}" ${TASK_EXISTS}`);
+  } else {
+    list.push({
+      name: taskName,
+      status: STATUS.TODO,
+      priority: PRIORITY.LOW,
+    });
+  }
 }
 
 function deleteTask(taskName) {
-  let index = list.findIndex((task) => task.name === taskName);
-
-  list.splice(index);
+  list = list.filter((task) => task.name !== taskName);
 }
 
 function showList() {
   let checkTask = {
-    taskCountTodo: false,
-    taskCountInProgress: false,
-    taskCountDone: false,
+    doesExistTodo: false,
+    doesExistInProgress: false,
+    doesExistDone: false,
   };
-
+  // TODO:
   console.log("Todo:");
   list.forEach((task) => {
     if (task.status === STATUS.TODO) {
       console.log(`\t "${task.name}",`);
-      checkTask.taskCountTodo = true;
+      checkTask.doesExistTodo = true;
     }
   });
 
-  if (checkTask.taskCountTodo == false) {
+  if (checkTask.doesExistTodo == false) {
     console.log(`\t ${DASH}`);
   }
-
+  // IN_PROGRESS:
   console.log("In Progress: ");
   list.forEach((task) => {
     if (task.status === STATUS.IN_PROGRESS) {
       console.log(`\t "${task.name}",`);
-      checkTask.taskCountInProgress = true;
+      checkTask.doesExistInProgress = true;
     }
   });
-  if (checkTask.taskCountInProgress == false) {
+  if (checkTask.doesExistInProgress == false) {
     console.log(`\t ${DASH}`);
   }
-
+  // DONE:
   console.log("Done: ");
   list.forEach((task) => {
     if (task.status === STATUS.DONE) {
       console.log(`\t "${task}",`);
-      checkTask.taskCountDone = true;
+      checkTask.doesExistDone = true;
     }
   });
-  if (checkTask.taskCountDone == false) {
+  if (checkTask.doesExistDone == false) {
     console.log(`\t ${DASH}`);
   }
 }
 
 addTask("create todo list");
 deleteTask("create todo list");
+addTask("drink water");
+changeStatus("drink water", STATUS.IN_PROGRESS);
 addTask("create a branch on Github");
-changeStatus("create a branch on Github", STATUS.IN_PROGRESS, PRIORITY.HIGH);
-changeStatus("write a post", STATUS.IN_PROGRESS, PRIORITY.LOW);
+changePriority("create a branch on Github", PRIORITY.HIGH);
+changeStatus("write a post", STATUS.IN_PROGRESS);
+changePriority("write a post", PRIORITY.LOW);
 
 showList();
