99d97516fd146346c5a066bb9e53f1186a138bb8
Viktor <victor.c715@gmail.com>
2022-11-05 16:48:36 +0300

    Other edits

--- viktorchern/main.js
+++ viktorchern/main.js
@@ -6,8 +6,17 @@
 // Используя рекурсивный setTimeout.
 
 function printNumbers(from, to){
-
-
+  let minNum = Number(from);
+  let maxNum = Number(to);
   
+  let timerId = setTimeout(function tick() {
+    if( minNum <= maxNum ){
+      console.log(minNum);
+      minNum++;
+      timerId = setTimeout(tick, 1000);
+    } else {
+      clearTimeout(timerId);
+    }
+  }, 1000);
 }
 printNumbers(3, 10);
