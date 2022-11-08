4fdbc6a7e54738f2a3d79f5edb775d191dda6c5b
Vadim Sysoev <sysoev.dev@gmail.com>
2022-11-09 00:57:48 +0300

    feat: Add UI_ELEMENTS object in JS

---
+++ main.js
@@ -0,0 +1,8 @@
+const UI_ELEMENTS = {
+  FORM: document.querySelector('.calc'),
+  NUMBER_1: document.querySelector('.calc__number--one'),
+  NUMBER_2: document.querySelector('.calc__number--two'),
+  OPERATION: document.querySelector('.calc__operation'),
+  BUTTON_SUBMIT: document.querySelector('.calc__btn-submit'),
+  OUTPUT: document.querySelector('.calc__output')
+}
