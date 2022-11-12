71ca05bf01aa3978b7704aadd1d0aa18bbc4612c
Danya Kolomiiets <115434508+prctmall@users.noreply.github.com>
2022-11-12 07:27:00 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+function calculate() {
+    let num1 = +(document.getElementById('num1')).value;
+    let num2 = +(document.getElementById('num2')).value;
+    let operator = document.querySelector('select').value;
+    let result = document.getElementById('result');
+    switch(operator) {
+        case 'add':
+            result.textContent = num1 + num2;
+            break;
+        case 'sub':
+            result.textContent = num1 - num2;
+            break;
+        case 'multi':
+            result.textContent = num1 * num2;
+            break;
+        default:
+            result.textContent = num1 / num2;
+    }
+}
