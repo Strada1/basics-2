44047b37edd866eccbdd55e51e522700296de9c2
Тамила Хасанова <tamosca@mail.ru>
2022-11-14 02:06:26 +0300

    Реализована функция сохранения результатов

--- Calculator/main.js
+++ Calculator/main.js
@@ -15,11 +15,13 @@ let firstNum = document.getElementById('firstNum');
 let doubleNum = document.getElementById('doubleNum');
 let out = document.getElementById('out')
 let calculate_button = document.getElementById('Calculate_button');
+let calc_div = document.getElementById('calc');
 
 firstNum.addEventListener('change',ReadFirstNum);
 doubleNum.addEventListener('change', ReadDoubleNum);
-calculate_button.addEventListener('click', Calc);
 choiceMain.addEventListener('change', ActionSelect);
+calculate_button.addEventListener('click', Calc);
+calculate_button.addEventListener('click', SaveResult);
 
 function ReadFirstNum()
 {
@@ -47,11 +49,13 @@ function Calc()
             result = Number(value1) + Number(value2);
             out.innerText = result;
             return result;
+            SaveResult();
             break;
         case Operations.Subtraction:
             result = value1 - value2;
             out.innerText = result;
             return result;
+            SaveResult();
             break;
         case Operations.Multi:
             result = value1 * value2;
@@ -64,4 +68,11 @@ function Calc()
             return result;
             break;
     }
+}
+
+function SaveResult()
+{
+    let div = document.createElement('div');
+    div.innerHTML = result;
+    document.body.append(div);
 }
