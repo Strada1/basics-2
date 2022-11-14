0aea8c2cf1c714c7ce6127c2bffc8db8867c8f5f
Nastya Shevchenko <anastasias859@gmail.com>
2022-11-14 14:27:41 +0300

    calc ui + module

---
+++ calcUI/add_deleteFn.js
@@ -0,0 +1,20 @@
+export let divRes = document.querySelector('.listResult');
+export let result = document.querySelector('.res');
+
+
+export function add() {
+    let el = document.createElement('div');
+    el.addEventListener('click', del);
+    el.innerHTML = result.innerHTML;
+    divRes.after(el); 
+
+function del() {
+    el.remove();
+
+    /*function del(el) {
+        el.target.remove();
+        // this.remove();
+    } */
+}
+};
+
