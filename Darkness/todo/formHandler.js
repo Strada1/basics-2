6ac711339a10e182fb62a76bb08cde7ec3cd6d34
iskander <iskandercapital@gmail.com>
2022-11-17 20:40:13 +0300

    TODO

--- Darkness/todo/formHandler.js
+++ Darkness/todo/formHandler.js
@@ -4,8 +4,9 @@ import { list } from "./list.js";
 import { render } from "./render.js";
 import { changeStatus } from "./list.js";
 import { removeTask } from "./list.js";
+import { sortList } from "./list.js";
 
-const formHandler = (event) => {
+export const formHandler = (event) => {
   event.preventDefault();
   const target = event.target;
   const inputName = target.children[0].name;
@@ -15,36 +16,30 @@ const formHandler = (event) => {
     return null;
   }
 
+  const listId = list.length > 0 ? sortList("id") + 1 : 1;
   addTask({
     name: inputValue,
     priority: inputName,
     status: STATUS.IN_PROGRESS,
+    id: listId,
   });
-  console.log(list);
+  sortList("status");
   render();
-  formsUpdate();
-};
-
-export const formsUpdate = () => {
-  const addTaskForms = document.querySelectorAll(".todo-main-elements-input");
-
-  for (let form of addTaskForms) {
-    form.addEventListener("submit", formHandler);
-  }
 };
 
 export const checkBoxHandle = (event) => {
   const checkboxButton = event.target;
   checkboxButton.classList.toggle("checked-button");
   event.target.parentElement.classList.toggle("checked");
-  const checkBoxValue = event.target.parentElement.children[1].outerText;
-  changeStatus(checkBoxValue);
+  const checkBoxId = +event.target.parentElement.dataset.id;
+  changeStatus(checkBoxId);
+  sortList("status");
+  render();
 };
 
 export const removeHandler = (event) => {
   const deleteButton = event.target;
-  const taskName = deleteButton.parentElement.children[1].outerText;
-  removeTask(taskName);
+  const taskId = +deleteButton.parentElement.dataset.id;
+  removeTask(taskId);
   render();
-  formsUpdate();
 };
--- Darkness/todo/list.js
+++ Darkness/todo/list.js
@@ -12,21 +12,25 @@ export let list = [
     name: `Вот вам и супер интересная тема.Вы наверняка заметили что ваши файлы с кодом становятся все объемнее, что хочется вынести некоторые вещи куда-то за пределы основной программы.`,
     priority: PRIORITY.HIGH,
     status: STATUS.IN_PROGRESS,
+    id: 2,
   },
   {
     name: "Сверстать этот TODO list",
     priority: PRIORITY.HIGH,
     status: STATUS.IN_PROGRESS,
+    id: 1,
   },
   {
     name: "Начать делать задачу",
     priority: PRIORITY.HIGH,
     status: STATUS.DONE,
+    id: 4,
   },
   {
     name: "Посмотреть ютубчик",
     priority: PRIORITY.LOW,
     status: STATUS.IN_PROGRESS,
+    id: 3,
   },
 ];
 
@@ -34,9 +38,8 @@ export const addTask = (task) => {
   list = [...list, task];
 };
 
-export const changeStatus = (taskName) => {
-  const result = list.filter((task) => task.name === taskName);
-
+export const changeStatus = (taskId) => {
+  const result = list.filter((task) => task.id === taskId);
   for (let task of result) {
     task.status =
       task.status === STATUS.IN_PROGRESS
@@ -45,10 +48,16 @@ export const changeStatus = (taskName) => {
         ? STATUS.IN_PROGRESS
         : null;
   }
-  console.log(list);
 };
 
-export const removeTask = (taskName) => {
-  list = list.filter((el) => !(el.name === taskName));
-  console.log(list);
+export const removeTask = (taskId) => {
+  list = list.filter((el) => !(el.id === taskId));
+};
+
+export const sortList = (category) => {
+  if (category === "status") {
+    list = [...list.sort((a, b) => (a.status > b.status ? -1 : 1))];
+  } else if (category === "id") {
+    return [...list.sort((a, b) => a.id - b.id)][list.length - 1].id;
+  }
 };
