3957f0bb6ae83dd8f0c9f0ffec12fd1f73a350df
Slava <slavaslayc@gmail.com>
2022-11-18 21:00:33 +0300

    fix add task with low priority

--- slavaslayc/main.js
+++ slavaslayc/main.js
@@ -2,21 +2,10 @@ const formHighTask = document.querySelector('#frmHighTask')
 const formLowTask = document.querySelector('#frmLowTask')
 const buttonLowTask = document.querySelector('#lowTask')
 const highTasks = document.querySelector('.highTaskList')
+const lowTasks = document.querySelector('.lowTaskList')
 
-
-// 1. Отключить действие отправки формы по умолчанию preventDefault
-// 2. Добавление задачи по нажатия кнопки и клавиши Enter
-// 3. Получить введенные данные из формы
-// 4. Создать div контейнер с задачей
-    // 1. В него добавить текст из формы
-    // 2. Чекбокс отмечающий задание
-    // 3. Кнопка удаления задачи
-// 5. Добавить получившуюся таску в нужный div контейнер
-
-
-
-function addTask () {
-    const inputValue = formHighTask[0].value;
+function addTask (taskPriority, targetForm) {
+    const inputValue = targetForm[0].value;
     const divElement = document.createElement('div');
     const paragraphElement = document.createElement('p');
     const textElement = document.createTextNode(inputValue)
@@ -31,10 +20,15 @@ function addTask () {
     paragraphElement.append(textElement);
     divElement.append(paragraphElement);
     divElement.append(deleteButton);
-    highTasks.append(divElement);
+    taskPriority.append(divElement);
 }
 
 formHighTask.addEventListener('submit', evt => {
     evt.preventDefault();
-    addTask()
+    addTask(highTasks, formHighTask)
+})
+
+formLowTask.addEventListener('submit', evt => {
+    evt.preventDefault();
+    addTask(lowTasks, formLowTask)
 })
