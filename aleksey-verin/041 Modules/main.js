be46517489da9954e9059d77afc6c1e355410a5d
Aleksey Verin <verevaa@yandex.ru>
2022-11-19 15:07:11 +0300

    Удалил старые файлы

--- aleksey-verin/041 Modules/main.js
+++
@@ -1,9 +0,0 @@
-import {calculate} from "./module-calculate.js";
-
-document.querySelector('#submitBtn').addEventListener("click", calculate)
-
-document.addEventListener("keypress", function(event) {
-    if (event.key === 'Enter') {
-        document.querySelector('#submitBtn').click()
-    }
-})
