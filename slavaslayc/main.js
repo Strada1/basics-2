ca6a719124266c7d2a83963354e0d6d71096bd7a
Slava <slavaslayc@gmail.com>
2022-11-20 16:33:11 +0300

    add todo ui

---
+++ slavaslayc/main.js
@@ -0,0 +1,70 @@
+const formHighTask = document.querySelector('#formHighTask');
+const formLowTask = document.querySelector('#formLowTask');
+const highTasks = document.querySelector('.highTaskList')
+const lowTasks = document.querySelector('.lowTaskList')
+
+const STATUS = {
+    IN_PROGRESS: 'In Progress',
+    DONE: 'Done',
+}
+
+const PRIORITY = {
+    HIGH: 'High',
+    LOW: 'Low',
+}
+
+let list = []
+
+function changeStatus () {
+
+}
+
+function addTaskList(event) {
+    event.preventDefault();
+    list.push(getFormData(event));
+    render();
+}
+
+function deleteTaskList (event) {
+    event.preventDefault();
+    const textDeleteTask = event.target.previousElementSibling.textContent
+    list = list.filter(task => task.name !== textDeleteTask)
+    render()
+}
+
+function getFormData (event) {
+    const taskText = event.target[0].value;
+    const taskPriority = event.target[0].getAttribute('data-priority');
+    return {name: taskText, status: STATUS.IN_PROGRESS, priority: taskPriority,};
+}
+
+function render () {
+    highTasks.innerHTML = '';
+    lowTasks.innerHTML = '';
+    list.forEach(item => {
+        const inputValue = item.name;
+        const divElement = document.createElement('div');
+        const paragraphElement = document.createElement('p');
+        const textElement = document.createTextNode(inputValue)
+        const checkBox = document.createElement('input')
+        const deleteButton = document.createElement('input')
+        checkBox.type = 'checkbox';
+        deleteButton.type = 'submit';
+        deleteButton.value = '';
+        deleteButton.className = 'deleteButton';
+        divElement.className = 'task';
+        divElement.append(checkBox);
+        paragraphElement.append(textElement);
+        divElement.append(paragraphElement);
+        divElement.append(deleteButton);
+        if (item.priority === PRIORITY.HIGH) {
+            highTasks.append(divElement);
+        } else if (item.priority === PRIORITY.LOW) {
+            lowTasks.append(divElement);
+        }
+        deleteButton.addEventListener('click', deleteTaskList)
+    });
+}
+
+formHighTask.addEventListener('submit', addTaskList)
+formLowTask.addEventListener('submit', addTaskList)
