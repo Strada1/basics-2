ba8462d05f56c6a2ba65edc6173c78833b0da4b0
Vladimir <wizart88@gmail.com>
2022-11-27 23:36:36 +0400

    feat: add try catch

--- 40day_good-speedf1_calc_UI/js/calc.js
+++ 40day_good-speedf1_calc_UI/js/calc.js
@@ -6,16 +6,29 @@ export const OPERATIONS = {
 };
 
 export function CalculateResult(firstOperand, secondOperand, operation) {
-	switch (operation) {
-		case OPERATIONS.PLUS:
-			return firstOperand + secondOperand;
-		case OPERATIONS.MINUS:
-			return firstOperand - secondOperand;
-		case OPERATIONS.MULTIPLY:
-			return firstOperand * secondOperand;
-		case OPERATIONS.DIVISION:
-			return firstOperand / secondOperand;
-		default:
-			break;
+
+	try {
+		switch (operation) {
+			case OPERATIONS.PLUS:
+				return firstOperand + secondOperand;
+			case OPERATIONS.MINUS:
+				return firstOperand - secondOperand;
+			case OPERATIONS.MULTIPLY:
+				return firstOperand * secondOperand;
+			case OPERATIONS.DIVISION:
+				if (+secondOperand === 0) {
+					console.log('divide on zero not allowed!!!');
+					throw new SyntaxError('divide on zero not allowed!!!')
+				}
+				return firstOperand / secondOperand;
+			default:
+				break;
+		}
+		
+	} catch (error) {
+		alert(error.message);
+		document.querySelector('.calc__number--two').focus();
+		return 'error';
 	}
+	
 }
