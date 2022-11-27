30124204780e6a558816acf806512cb403c2e41c
Vadim Sysoev <sysoev.dev@gmail.com>
2022-11-27 17:13:18 +0300

    Fix: Fix CSS styles

--- js/main.js
+++ js/main.js
@@ -1,10 +1,4 @@
 import { UI_ELEMENTS } from "./ui.js";
 
-UI_ELEMENTS.TABS.forEach((item) => {
-  item.addEventListener('click', () => {
-    UI_ELEMENTS.TABS.forEach((item) => {
-      item.classList.remove('tabs-item--active')
-    })
-    item.classList.add('tabs-item--active')
-  })
-})
+window.location.hash = "now";
+
--- js/ui.js
+++ js/ui.js
@@ -1,3 +1,17 @@
 export const UI_ELEMENTS = {
-  TABS: document.querySelectorAll('.tabs-item')
+  TABS: document.querySelectorAll('.tabs-item'),
+  BTN_LIKE: document.querySelector('.now__sities-btn')
 }
+
+UI_ELEMENTS.TABS.forEach((item) => {
+  item.addEventListener('click', () => {
+    UI_ELEMENTS.TABS.forEach((item) => {
+      item.classList.remove('tabs-item--active')
+    })
+    item.classList.add('tabs-item--active')
+  })
+})
+
+UI_ELEMENTS.BTN_LIKE.addEventListener('click', () => {
+  UI_ELEMENTS.BTN_LIKE.classList.toggle('now__sities-btn--like')
+})
