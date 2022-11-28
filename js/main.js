85aaaa0511e50cffd1cef3c15858d06cabba7361
Vadim Sysoev <sysoev.dev@gmail.com>
2022-11-28 18:48:06 +0300

    feat: Сделал отображение экрана Now

--- js/main.js
+++ js/main.js
@@ -1,7 +1,18 @@
-import { UI_ELEMENTS } from "./ui.js";
+import { UI_ELEMENTS, showWeatherNow } from "./ui.js";
 window.location.hash = "now";
 
 UI_ELEMENTS.FORM.addEventListener('submit', (event) => {
   event.preventDefault();
-  alert('test')
+  const cityName = UI_ELEMENTS.INPUT_FORM.value;
+  getWeather(cityName);
+  event.target.reset();
 })
+
+async function getWeather(cityName) {
+  const SERVER_URL = 'http://api.openweathermap.org/data/2.5/weather';
+  const API_KEY = 'f660a2fb1e4bad108d6160b7f58c555f';
+  const url = `${SERVER_URL}?q=${cityName}&appid=${API_KEY}&units=metric`;
+  const promise = await fetch(url);
+  const data = await promise.json();
+  showWeatherNow(data);
+}
--- js/ui.js
+++ js/ui.js
@@ -1,7 +1,10 @@
 export const UI_ELEMENTS = {
   FORM: document.querySelector('.weather__search-form'),
+  INPUT_FORM: document.querySelector('.weather__search-input'),
   TABS: document.querySelectorAll('.tabs-item'),
-  BTN_LIKE: document.querySelector('.now__sities-btn')
+  BTN_LIKE: document.querySelector('.now__sities-btn'),
+  NOW_TEMPERATURE: document.querySelector('.now__temperature'),
+  NOW_CITY: document.querySelector('.now__sities'),
 }
 
 UI_ELEMENTS.TABS.forEach((item) => {
@@ -16,3 +19,8 @@ UI_ELEMENTS.TABS.forEach((item) => {
 UI_ELEMENTS.BTN_LIKE.addEventListener('click', () => {
   UI_ELEMENTS.BTN_LIKE.classList.toggle('now__sities-btn--like')
 })
+
+export function showWeatherNow(data) {
+  UI_ELEMENTS.NOW_TEMPERATURE.textContent = data.main.temp.toFixed(1);
+  UI_ELEMENTS.NOW_CITY.textContent = data.name;
+}
