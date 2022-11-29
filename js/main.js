a1f7336974969429d33dc3f222df96573694894e
Vadim Sysoev <sysoev.dev@gmail.com>
2022-11-29 16:48:21 +0300

    feat: complete 56 task

--- js/main.js
+++ js/main.js
@@ -1,6 +1,18 @@
 import { UI_ELEMENTS, showWeatherNow } from "./ui.js";
 window.location.hash = "now";
 
+// const favoriteList = [];
+
+// function addCityFavorite() {
+//   // функция добавления города в избранное
+//   console.log('test');
+// }
+
+// UI_ELEMENTS.NOW_BTN_LIKE.addEventListener('click', addCityFavorite);
+
+function showError(error) {
+  alert(error)
+}
 UI_ELEMENTS.FORM.addEventListener('submit', (event) => {
   event.preventDefault();
   const cityName = UI_ELEMENTS.INPUT_FORM.value;
@@ -13,6 +25,8 @@ async function getWeather(cityName) {
   const API_KEY = 'f660a2fb1e4bad108d6160b7f58c555f';
   const url = `${SERVER_URL}?q=${cityName}&appid=${API_KEY}&units=metric`;
   const promise = await fetch(url);
-  const data = await promise.json();
+  // promise.catch(err => alert(err))
+  const data = await promise.json()
   showWeatherNow(data);
 }
+
--- js/ui.js
+++ js/ui.js
@@ -2,10 +2,13 @@ export const UI_ELEMENTS = {
   FORM: document.querySelector('.weather__search-form'),
   INPUT_FORM: document.querySelector('.weather__search-input'),
   TABS: document.querySelectorAll('.tabs-item'),
-  BTN_LIKE: document.querySelector('.now__sities-btn'),
   NOW_TEMPERATURE: document.querySelector('.now__temperature'),
   NOW_CITY: document.querySelector('.now__sities'),
+  NOW_ICON: document.querySelector('.now__icon'),
+  NOW_BTN_LIKE: document.querySelector('.now__sities-btn'),
 }
+
+let favoriteList = [];
 
 UI_ELEMENTS.TABS.forEach((item) => {
   item.addEventListener('click', () => {
@@ -16,11 +19,28 @@ UI_ELEMENTS.TABS.forEach((item) => {
   })
 })
 
-UI_ELEMENTS.BTN_LIKE.addEventListener('click', () => {
-  UI_ELEMENTS.BTN_LIKE.classList.toggle('now__sities-btn--like')
-})
+UI_ELEMENTS.NOW_BTN_LIKE.addEventListener('click', () => {
+  const cityName = UI_ELEMENTS.NOW_BTN_LIKE.previousElementSibling.textContent;
+  UI_ELEMENTS.NOW_BTN_LIKE.classList.toggle('now__sities-btn--like');
+  if (UI_ELEMENTS.NOW_BTN_LIKE.className === 'now__sities-btn now__sities-btn--like') {
+    favoriteList.push(cityName)
+  } else {
+    favoriteList = favoriteList.filter((item) => {
+      return item != cityName;
+    })
+  }
+
+  console.log(favoriteList);
+});
+
+function checkFavoriteList() {
+
+}
 
 export function showWeatherNow(data) {
+  const iconUrl = `https://openweathermap.org/img/wn/${data.weather[0].icon}@4x.png`;
   UI_ELEMENTS.NOW_TEMPERATURE.textContent = data.main.temp.toFixed(1);
   UI_ELEMENTS.NOW_CITY.textContent = data.name;
+  UI_ELEMENTS.NOW_ICON.setAttribute('src', iconUrl)
 }
+
