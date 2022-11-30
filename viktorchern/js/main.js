f4d64b8931ea0715b35a59844c9ec14f090ad439
Viktor <victor.c715@gmail.com>
2022-11-30 13:25:19 +0300

    Weather lesson 57

---
+++ viktorchern/js/main.js
@@ -0,0 +1,68 @@
+import { UI_ELEMENTS } from './ui.js';
+import { ERRORS } from './errors.js';
+
+// Tabs
+function tab(event){
+  const TAB_ID = this.dataset.tab;
+
+  UI_ELEMENTS.TAB__ITEMS.forEach( item => item.style.display = 'none' );
+  document.querySelector('.tab__item[data-tab="'+TAB_ID+'"]').style.display = 'flex';
+
+  UI_ELEMENTS.TAB_BUTTONS.forEach( item => item.classList.remove('tab__button_active') );
+  this.classList.add('tab__button_active');
+}
+UI_ELEMENTS.TAB_BUTTONS.forEach( item => item.addEventListener('click', tab) );
+
+function createWeatherImg(icon, alt){
+  const IMG = document.createElement('img');
+  IMG.setAttribute('alt', alt);
+  IMG.setAttribute('src', `https://openweathermap.org/img/wn/${icon}@2x.png`);
+  return IMG;
+}
+
+function showWeather(event){
+  event.preventDefault();
+  const serverUrl = 'http://api.openweathermap.org/data/2.5/weather';
+  const cityName = UI_ELEMENTS.FORM_INPUT.value;
+  const apiKey = 'f660a2fb1e4bad108d6160b7f58c555f';
+  const url = `${serverUrl}?q=${cityName}&appid=${apiKey}&units=metric`;
+  
+  try {
+
+    if( cityName === '' ) {
+      throw new SyntaxError(ERRORS.INPUT_EMPTY);
+    } else if ( !cityName ) {
+      throw new SyntaxError(ERRORS.INPUT_ERROR);
+    }
+
+    fetch(url)
+      .then(
+        response => {
+          try {
+            if ( !response.ok ) {
+              throw new SyntaxError(ERRORS.FETCH_ERROR);
+            }
+            return response.json();
+            
+          } catch( err ) {
+            alert(err.message);
+          }
+        }
+      )
+      .then( data => {
+        const IMG_ALT = data.weather[0].description;
+        const IMG_ICON = data.weather[0].icon;
+
+        console.log(data);
+        UI_ELEMENTS.TAB_NOW_LOCATION.textContent = data.name;
+        UI_ELEMENTS.TAB_NOW_TEMP.textContent = `${Math.floor(data.main.temp)}°`;
+
+        UI_ELEMENTS.TAB_NOW_IMG.replaceWith(createWeatherImg(IMG_ICON, IMG_ALT))
+      } );
+
+
+  } catch(err) {
+    alert(err.message);
+  }
+}
+UI_ELEMENTS.FORM_BUTTON.addEventListener('click', showWeather);
---
+++ viktorchern/js/ui.js
@@ -0,0 +1,10 @@
+export const UI_ELEMENTS = {
+  TAB: document.querySelector('#tabs'),
+  TAB__ITEMS: document.querySelectorAll('.tab__item'),
+  TAB_BUTTONS: document.querySelectorAll('.tab__button'),
+  FORM_INPUT: document.querySelector('#weather-input'),
+  FORM_BUTTON: document.querySelector('#weather-search'),
+  TAB_NOW_LOCATION: document.querySelector('.tab__item_now .tab__location'),
+  TAB_NOW_TEMP: document.querySelector('.tab__item_now .tab__title'),
+  TAB_NOW_IMG: document.querySelector('.tab__item_now .tab__img'),
+}
