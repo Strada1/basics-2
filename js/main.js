0334222ec82b545c4546b3a707201b53b390202e
Vadim Sysoev <sysoev.dev@gmail.com>
2022-12-01 18:11:26 +0300

    feat: Complete 60 task

--- js/main.js
+++ js/main.js
@@ -1,6 +1,9 @@
 import { UI_ELEMENTS, showWeatherNow, showWeatherDetails } from "./ui.js";
+import { addStorageCurrentCity, getStorageCurrentCity, addStorageFavoriteCities, getStorageFavoriteCities } from "./storage.js";
 window.location.hash = "now";
 
+const SERVER_URL = 'http://api.openweathermap.org/data/2.5/weather';
+const API_KEY = 'f660a2fb1e4bad108d6160b7f58c555f';
 let favoriteCityList = [];
 
 UI_ELEMENTS.NOW_BTN_LIKE.addEventListener('click', () => {
@@ -8,16 +11,33 @@ UI_ELEMENTS.NOW_BTN_LIKE.addEventListener('click', () => {
   addCityFavorite(cityName);
 });
 
+function addCityFavorite(cityName) {
+  const isValid = favoriteCityList.includes(cityName);
+  if (isValid) {
+    favoriteCityList = favoriteCityList.filter((item) => {
+      return item != cityName;
+    });
+    renderFavoriteCity();
+  } else {
+    favoriteCityList.push(cityName);
+    addStorageFavoriteCities(favoriteCityList)
+    renderFavoriteCity();
+    console.log(favoriteCityList);
+  }
+}
+
 function deleteFavoriteCity() {
   const cityName = this.parentNode.textContent
   this.parentNode.remove();
   favoriteCityList = favoriteCityList.filter(item => {
     return item != cityName;
-  })
+  });
+  addStorageFavoriteCities(favoriteCityList)
 }
 
 function renderFavoriteCity() {
   UI_ELEMENTS.FAVORITE_LIST.textContent = '';
+  favoriteCityList = getStorageFavoriteCities();
   favoriteCityList.forEach(element => {
     const isValid = favoriteCityList.includes(element);
     const item = document.createElement('li');
@@ -33,20 +53,9 @@ function renderFavoriteCity() {
     UI_ELEMENTS.FAVORITE_LIST.append(item);
     item.addEventListener('click', () => {
       getWeather(element);
+      addStorageCurrentCity(element);
     })
   });
-}
-
-function addCityFavorite(cityName) {
-  const isValid = favoriteCityList.includes(cityName);
-  if (isValid) {
-    favoriteCityList = favoriteCityList.filter((item) => {
-      return item != cityName;
-    })
-  } else {
-    favoriteCityList.push(cityName);
-    renderFavoriteCity()
-  }
 }
 
 function checkCityFavorite(cityName) {
@@ -61,14 +70,21 @@ function checkCityFavorite(cityName) {
 UI_ELEMENTS.FORM.addEventListener('submit', (event) => {
   event.preventDefault();
   const cityName = UI_ELEMENTS.INPUT_FORM.value;
+  addStorageCurrentCity(cityName)
   getWeather(cityName);
   event.target.reset();
 })
 
 async function getWeather(cityName) {
-  const SERVER_URL = 'http://api.openweathermap.org/data/2.5/weather';
-  const API_KEY = 'f660a2fb1e4bad108d6160b7f58c555f';
-  const url = `${SERVER_URL}?q=${cityName}&appid=${API_KEY}&units=metric`;
+  let city;
+  if (!cityName) {
+    city = getStorageCurrentCity() || 'Aktobe';
+  } else {
+    city = cityName
+  }
+
+  const url = `${SERVER_URL}?q=${city}&appid=${API_KEY}&units=metric`;
+
   try {
     const promise = await fetch(url);
     const data = await promise.json();
@@ -87,3 +103,7 @@ export function convertTime(time) {
   const result = `${hours}:${minutes}`;
   return result;
 }
+
+getWeather();
+getStorageFavoriteCities();
+renderFavoriteCity();
