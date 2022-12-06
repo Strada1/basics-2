df99ecae7496528eecc1006aa7069e7560fb0c9e
Nastya Shevchenko <anastasias859@gmail.com>
2022-12-06 10:53:55 +0300

    added js tabs, on my way to refactoring

--- Weather/main.js
+++ Weather/main.js
@@ -1,4 +1,7 @@
-
+const tabs = {
+    allBtn: document.querySelectorAll('.btn-nav'),
+    allTabs: document.querySelectorAll('.tabs_block'),
+}
 const formElements = {
     mainForm: document.querySelector('.main-form'),
     cityInput: document.querySelector('.city-input'),
@@ -20,6 +23,18 @@ const detailsBlockEl = {
     detailsSunset: document.querySelector('.det-sunset'),
 }
 let divList = document.querySelector('.div-list');
+let listArr = [];
+if(localStorage.getItem('listArr')) {
+    listArr = JSON.parse(localStorage.getItem('listArr'));
+    showList();
+}
+
+function showList() {
+    divList.innerHTML = '';
+    listArr.forEach((item)=> {
+        createCityEl(item);
+    });
+};
 
 formElements.mainForm.addEventListener('submit', (event) =>{
     event.preventDefault();
@@ -28,52 +43,27 @@ formElements.mainForm.addEventListener('submit', (event) =>{
     formElements.cityInput.value = '';
 });
 
-function getRequest(link, cityName) {
-    fetch( `${link}?q=${cityName}&appid=${apiKey}`)
-    .then(response => {
-        if(response.ok) {
-            return response.json()
-        } else {
-            alert("Ошибка HTTP: " + response.status);
-        }
-        response.json()})
-    .then(data => {
-        renderData(data);
-        renderDetails(data)
-    });
-};
-
-function getCelsius(num) {
-    try{
-        if(isNaN(num)) throw new SyntaxError('Ошибка');
-        let res = Math.floor(num - 273.15);
-        return res;
-    } catch(err) {
-        console.log('Ошибка');
-    }
-};
-
-function renderData(value) {
-    nowBlockEl.nowDegrees.textContent = getCelsius(value.main.temp) + '℃';
-    nowBlockEl.nowCity.textContent = value.name;
-    nowBlockEl.nowIcon.src = `http://openweathermap.org/img/wn/${value.weather[0].icon}@4x.png`;
-};
-
-function renderDetails(value) {
-    detailsBlockEl.detailsCity.textContent = value.name;
-    detailsBlockEl.detailsTemp.textContent = getCelsius(value.main.temp) + '℃';
-    detailsBlockEl.detailsFeel.textContent = getCelsius(value.main.feels_like) + '℃';
-    detailsBlockEl.detailsWeather.textContent = value.weather[0].main;
-    detailsBlockEl.detailsSunrise.textContent = new Date(1000 * value.sys.sunrise);
-    detailsBlockEl.detailsSunset.textContent = new Date(1000 * value.sys.sunset);
-}
-
-let listArr = [];
-
-if(localStorage.getItem('listArr')) {
-    listArr = JSON.parse(localStorage.getItem('listArr'));
+nowBlockEl.nowHeart.addEventListener('click', () =>{
+    addToarr(nowBlockEl.nowCity.textContent);
+    localStorage.setItem('listArr', JSON.stringify(listArr));
     showList();
-}
+});
+
+tabs.allBtn.forEach(function(item) {
+    item.addEventListener('click', function() {
+        let currentBtn = item;
+        let tabId = currentBtn.getAttribute('data-tab');
+        let currentTab = document.querySelector(tabId)
+        tabs.allBtn.forEach(function(item) {
+            item.classList.remove('active');
+        });
+        tabs.allTabs.forEach(function(item) {
+            item.classList.remove('active');
+        });
+        currentBtn.classList.add('active');
+        currentTab.classList.add('active');
+    })
+});
 
 function addToarr(name) {
     try{
@@ -90,7 +80,6 @@ function addToarr(name) {
     } catch(err) {
         console.log('Ошибка');
     }
-    console.log(listArr)
 };
 
 function toggleLike(name) {
@@ -101,6 +90,54 @@ function toggleLike(name) {
     }
 };
 
+function deleteFromArr(cityName) {
+    let res = listArr.findIndex(function(item) {
+        return cityName == item;
+    })
+    listArr.splice(res, 1);
+    localStorage.setItem('listArr', JSON.stringify(listArr))
+    showList()
+};
+
+function getRequest(link, cityName) {
+    fetch( `${link}?q=${cityName}&appid=${apiKey}`)
+    .then(response => {
+        if(response.ok) {
+            return response.json()
+        } else {
+            alert("Ошибка HTTP: " + response.status);
+        }
+        response.json()})
+    .then(data => {
+        renderData(data);
+        renderDetails(data)
+    });
+};
+
+function renderData(value) {
+    nowBlockEl.nowDegrees.textContent = getCelsius(value.main.temp) + '℃';
+    nowBlockEl.nowCity.textContent = value.name;
+    nowBlockEl.nowIcon.src = `http://openweathermap.org/img/wn/${value.weather[0].icon}@4x.png`;
+};
+
+function renderDetails(value) {
+    detailsBlockEl.detailsCity.textContent = value.name;
+    detailsBlockEl.detailsTemp.textContent = getCelsius(value.main.temp) + '℃';
+    detailsBlockEl.detailsFeel.textContent = getCelsius(value.main.feels_like) + '℃';
+    detailsBlockEl.detailsWeather.textContent = value.weather[0].main;
+    detailsBlockEl.detailsSunrise.textContent = new Date(1000 * value.sys.sunrise);
+    detailsBlockEl.detailsSunset.textContent = new Date(1000 * value.sys.sunset);
+};
+
+function getCelsius(num) {
+    try{
+        if(isNaN(num)) throw new SyntaxError('Ошибка');
+        let res = Math.floor(num - 273.15);
+        return res;
+    } catch(err) {
+        console.log('Ошибка');
+    }
+};
 
 function createCityEl(name) {
     let newEl = document.createElement('li');
@@ -121,25 +158,3 @@ function createCityEl(name) {
         deleteFromArr(name);
     });
 };
-
-function deleteFromArr(cityName) {
-    let res = listArr.findIndex(function(item) {
-        return cityName == item;
-    })
-    listArr.splice(res, 1);
-    localStorage.setItem('listArr', JSON.stringify(listArr))
-    showList()
-};
-
-function showList() {
-    divList.innerHTML = '';
-    listArr.forEach((item)=> {
-        createCityEl(item);
-    })
-};
-
-nowBlockEl.nowHeart.addEventListener('click', () =>{
-    addToarr(nowBlockEl.nowCity.textContent);
-    localStorage.setItem('listArr', JSON.stringify(listArr));
-    showList();
-});
