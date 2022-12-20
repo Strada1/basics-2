b78b766d438be06f1da927db22f88413b4dbcc36
johnbeelow <beelow-dj@mail.ru>
2022-12-20 21:40:34 +0300

    TODO Constr

--- beelow/js/main.js
+++ beelow/js/main.js
@@ -1,387 +1,137 @@
+// Попробуйте применить это в “TODO” (создание объекта задачи) или “Погоде” (создание объекта города в избранном)
+
+//Ваш код будет выглядеть примерно так:
+//const tasks = new Task(’переписать TODO с конструктором’)
+
+//А есть ли в этом хоть какой-то смысл?  Да, определенно есть. И не только когда вы пробуете писать в ООП стиле. Даже на примере такого простого объекта как Tasks можно увидеть что для новой задачи вам не нужно передавать ее статус, а это уменьшает кол-во ошибок при создании новых задач, а еще уменьшает кол-во кода. Уменьшение кол-ва кода - лучший способ для уменьшения кол-ва багов. Абсолютно серьезно, чем больше шансов для ошибок - тем больше ошибок.
+
+// TODO из Lesson #45 (01_basics)
+
 const UI_ELEMENTS = {
-  MENU_NOW: document.querySelector(".left_menu__page_one"),
-  MENU_DETAILS: document.querySelector(".left_menu__page_two"),
-  MENU_FORECAST: document.querySelector(".left_menu__page_three"),
-  BUTTON_NOW: document.querySelector(".container__now"),
-  BUTTON_DETAILS: document.querySelector(".container__details"),
-  BUTTON_FORECAST: document.querySelector(".container__forecast"),
-  INPUT_FORM: document.querySelector(".input_form"),
-  INPUT_TEXT: document.querySelector(".container__search_city__input"),
-  LEFT_MENU_PAGE_NEW: document.querySelector(".left_menu__page_one"),
-  NOW_INFO_TEMPERATURE: document.querySelector(".page_one__info__temperature"),
-  NOW_IMG_WEATHER: document.querySelector(".page_one__info__sign"),
-  NOW_CITY_NAME: document.querySelector(".page_one__info__city"),
-  NOW_IMG_HEART: document.querySelector(".page_one__info__heart"),
-  CITY_LIST: document.querySelector(".city_list"),
-  DETAIL_CITY_NAME: document.querySelector(".page_two__info__city"),
-  DETAIL_INFO_LIST: document.querySelector(".page_two__info__list"),
-  DETAIL_LIST_TEMP: document.querySelector(".detail_list_temp"),
-  DETAIL_LIST_FEELS: document.querySelector(".detail_list_feels"),
-  DETAIL_LIST_WEATHER: document.querySelector(".detail_list_weather"),
-  DETAIL_LIST_SUNRISE: document.querySelector(".detail_list_sunrise"),
-  DETAIL_LIST_SUNSET: document.querySelector(".detail_list_sunset"),
-  FORECAST_CITY_NAME: document.querySelector(".page_three__info__city"),
-  FORECAST_CARD_LIST: document.querySelector(".forecast__card_list"),
+  ADD_TASK_HIGHT: document.querySelector(".button_add_task_hight"),
+  ADD_TASK_LOW: document.querySelector(".button_add_task_low"),
+  INPUT_TEXT_HIGHT: document.querySelector(".input_task_hight"),
+  INPUT_TEXT_LOW: document.querySelector(".input_task_low"),
+  INPUT_FORM_HIGHT: document.querySelector(".input_form_hight"),
+  INPUT_FORM_LOW: document.querySelector(".input_form_low"),
+  PUSH_PRIORITY_HIGHT: document.querySelector(".hight_task_container"),
+  PUSH_PRIORITY_LOW: document.querySelector(".low_task_container"),
 };
 
-const CREATE_ELEMENTS = {
-  DIV: function () {
-    return document.createElement("div");
-  },
-  A: function () {
-    return document.createElement("a");
-  },
-  LI: function () {
-    return document.createElement("li");
-  },
-  P: function () {
-    return document.createElement("p");
-  },
-  BR: function () {
-    return document.createElement("br");
-  },
-  IMG: function () {
-    return document.createElement("img");
-  },
+const CHECK = {
+  CHANGE_DONE: "Задача выполнена",
+  CHANGE_DEFAULT: "Задача не выполнена",
+  CHANGE_PRIORITY: "Приоритет изминен",
+  ADD_TASK_MASSIVE: "[MASSIVE] - Добавлена новая задача",
+  DELETE_TASK_MASSIVE: "[MASSIVE] - Задача уадалена",
+  IS_RENDER: "[RENDER] - Complete",
 };
 
-
-const API = {
-  URL_WEATHER: "https://api.openweathermap.org/data/2.5/weather",
-  URL_IMG: "http://openweathermap.org/img/wn",
-  URL_FORECAST: "https://api.openweathermap.org/data/2.5/forecast",
-  KEY: "0515a87e363fac066d60e5e7cd8721be",
-  IMG_SIZE_2X: "@2x.png",
+const PRIORITY = {
+  HIGHT: "Hight",
+  LOW: "Low",
 };
 
+const STATUS = {
+  DONE: "Done",
+  TO_DO: "To Do",
+};
 
-let currentCity = "London";
-let cityFavoriteList = [];
+const taskList = [];
 
-UI_ELEMENTS.BUTTON_NOW.addEventListener("click", swapOnNow);
-UI_ELEMENTS.BUTTON_DETAILS.addEventListener("click", swapOnDetails);
-UI_ELEMENTS.BUTTON_FORECAST.addEventListener("click", swapOnForecast);
+const DEFAULT_STATUS = STATUS.TO_DO;
 
-UI_ELEMENTS.INPUT_FORM.addEventListener("submit", function (event) {
-  event.preventDefault();
-  getData(UI_ELEMENTS.INPUT_TEXT.value);
-  UI_ELEMENTS.INPUT_FORM.reset();
-});
-
-UI_ELEMENTS.NOW_IMG_HEART.addEventListener("click", function () {
-  addCityFavorite(currentCity);
-});
-
-document.addEventListener("DOMContentLoaded", getContnetLoaded);
-
-function getContnetLoaded() {
-  currentCity = storage.getCurrentCity();
-  cityFavoriteList = storage.getCityFavoriteList();
-  getWeatherData(currentCity);
-  getForecastData(currentCity);
-  renderAdedLocations();
+function Constructor(nameTask, priorityTask) {
+  this.name = nameTask;
+  this.status = DEFAULT_STATUS;
+  this.priority = priorityTask;
 }
 
-
-const storage = {
-  saveCityFavoriteList: function (cityFavoriteList) {
-    localStorage.setItem("cityFavoriteList", JSON.stringify(cityFavoriteList));
-  },
-  saveCurrentCity: function (currentCity) {
-    localStorage.setItem("currentCity", currentCity);
-  },
-  getCityFavoriteList: function () {
-    return JSON.parse(localStorage.getItem("cityFavoriteList"));
-  },
-  getCurrentCity: function () {
-    return localStorage.getItem("currentCity");
-  },
-};
-
-
-function addCityFavorite(newCityFavorite) {
-  let findName = cityFavoriteList.findIndex(function (findName) {
-    return findName.name === newCityFavorite;
+function addTask(nameTask, priorityTask) {
+  let findName = taskList.findIndex(function (findName) {
+    return findName.name === nameTask;
   });
   if (findName >= 0) {
-    alert("Такой город уже есть");
     return;
-  } else if (newCityFavorite === "") {
-    alert("Введите название города");
   } else {
-    cityFavoriteList.push({
-      name: newCityFavorite,
-    });
+    let task = new Constructor(nameTask, priorityTask);
+    taskList.push(task);
   }
-  renderAdedLocations();
+  render();
 }
 
-function cityFavoriteToLeftTab(name) {
-  getWeatherData(name);
-  getForecastData(name);
+function deleteTask(idTask) {
+  taskList.splice(idTask, 1);
+  console.log(CHECK.DELETE_TASK_MASSIVE);
+  render();
 }
 
-function deleteCity(idTask) {
-  cityFavoriteList.splice(idTask, 1);
-  console.log("Удален из избранного");
-  renderAdedLocations();
-}
-
-
-function getWeatherData(cityName) {
-  const url = `${API.URL_WEATHER}?q=${cityName}&appid=${API.KEY}`;
-  fetch(url)
-    .then((response) => response.json())
-    .then((data) => {
-      updateCurrentCity(data);
-      parseWeatherElements(data);
-    })
-    .catch(alert);
-}
-
-function getForecastData(cityName) {
-  const url = `${API.URL_FORECAST}?q=${cityName}&appid=${API.KEY}`;
-  fetch(url)
-    .then((response) => response.json())
-    .then((data) => {
-      parseForecastElements(data);
-      console.log(data);
-    })
-    .catch(alert);
-}
-
-function parseWeatherElements({ name, main, weather, sys }) {
-  const nameCity = name;
-  const temperature = convertKelvinToCelsius(main.temp);
-  const imgWeather = `${API.URL_IMG}/${weather[0].icon}${API.IMG_SIZE_2X}`;
-  const feelsLike = convertKelvinToCelsius(main.feels_like);
-  const readWeather = weather[0].main;
-  const sunrise = convertUnixToTime(sys.sunrise);
-  const sunset = convertUnixToTime(sys.sunset);
-
-  renderNow(temperature, imgWeather, nameCity);
-  renderDetail(nameCity, temperature, feelsLike, readWeather, sunrise, sunset);
-}
-
-function parseForecastElements({ city, list }) {
-  UI_ELEMENTS.FORECAST_CARD_LIST.innerHTML = "";
-
-  const nameCity = city.name;
-
-  for (let element of list) {
-    const date = convertUnixToDate(element.dt);
-    const time = convertUnixToTime(element.dt);
-    const temperature = convertKelvinToCelsius(element.main.temp);
-    const feelsLike = convertKelvinToCelsius(element.main.feels_like);
-    const textWeather = element.weather[0].main;
-    const imgWeather = `${API.URL_IMG}/${element.weather[0].icon}.png`;
-
-    renderForecast(
-      nameCity,
-      date,
-      time,
-      temperature,
-      feelsLike,
-      textWeather,
-      imgWeather
-    );
+function changeStatus(idTask) {
+  if (taskList[idTask].status == STATUS.TO_DO) {
+    return (taskList[idTask].status = STATUS.DONE);
+  } else if (taskList[idTask].status == STATUS.DONE) {
+    return (taskList[idTask].status = STATUS.TO_DO);
   }
+  console.log(CHECK.CHANGE_DONE);
+  render();
 }
 
-function updateCurrentCity({ name }) {
-  currentCity.length = 0;
-  currentCity = name;
-  storage.saveCurrentCity(name);
-}
+function pushTaskUi(task, index, taskRender) {
+  let divTask = document.createElement("div");
+  let nameTask = document.createElement("label");
+  let checkStatus = document.createElement("input");
+  let deleteButton = document.createElement("div");
 
-function getData(cityName) {
-  getWeatherData(cityName);
-  getForecastData(cityName);
-}
+  divTask.className = "task";
+  divTask.id = index;
+  checkStatus.className = "checkbox";
+  checkStatus.type = "checkbox";
+  deleteButton.className = "button_delete_task";
+  deleteButton.textContent = "×";
 
-
-function swapOnNow() {
-  UI_ELEMENTS.MENU_NOW.hidden = false;
-  UI_ELEMENTS.MENU_DETAILS.hidden = true;
-  UI_ELEMENTS.MENU_FORECAST.hidden = true;
-
-  UI_ELEMENTS.BUTTON_NOW.style.background = "#000";
-  UI_ELEMENTS.BUTTON_NOW.style.color = "#fff";
-  UI_ELEMENTS.BUTTON_NOW.style.border = "1px solid #000000";
-  UI_ELEMENTS.BUTTON_NOW.style.cursor = "pointer";
-
-  UI_ELEMENTS.BUTTON_DETAILS.style.background = "#cac7be";
-  UI_ELEMENTS.BUTTON_DETAILS.style.color = "#000";
-  UI_ELEMENTS.BUTTON_DETAILS.style.border = "1px solid #000000";
-  UI_ELEMENTS.BUTTON_DETAILS.style.cursor = "pointer";
-
-  UI_ELEMENTS.BUTTON_FORECAST.style.background = "#cac7be";
-  UI_ELEMENTS.BUTTON_FORECAST.style.color = "#000";
-  UI_ELEMENTS.BUTTON_FORECAST.style.border = "1px solid #000000";
-  UI_ELEMENTS.BUTTON_FORECAST.style.cursor = "pointer";
-}
-
-function swapOnDetails() {
-  UI_ELEMENTS.MENU_NOW.hidden = true;
-  UI_ELEMENTS.MENU_DETAILS.hidden = false;
-  UI_ELEMENTS.MENU_FORECAST.hidden = true;
-
-  UI_ELEMENTS.BUTTON_NOW.style.background = "#cac7be";
-  UI_ELEMENTS.BUTTON_NOW.style.color = "#000";
-  UI_ELEMENTS.BUTTON_NOW.style.border = "1px solid #000000";
-  UI_ELEMENTS.BUTTON_NOW.style.cursor = "pointer";
-
-  UI_ELEMENTS.BUTTON_DETAILS.style.background = "#000";
-  UI_ELEMENTS.BUTTON_DETAILS.style.color = "#fff";
-  UI_ELEMENTS.BUTTON_DETAILS.style.border = "1px solid #000000";
-  UI_ELEMENTS.BUTTON_DETAILS.style.cursor = "pointer";
-
-  UI_ELEMENTS.BUTTON_FORECAST.style.background = "#cac7be";
-  UI_ELEMENTS.BUTTON_FORECAST.style.color = "#000";
-  UI_ELEMENTS.BUTTON_FORECAST.style.border = "1px solid #000000";
-  UI_ELEMENTS.BUTTON_FORECAST.style.cursor = "pointer";
-}
-
-function swapOnForecast() {
-  UI_ELEMENTS.MENU_NOW.hidden = true;
-  UI_ELEMENTS.MENU_DETAILS.hidden = true;
-  UI_ELEMENTS.MENU_FORECAST.hidden = false;
-
-  UI_ELEMENTS.BUTTON_NOW.style.background = "#cac7be";
-  UI_ELEMENTS.BUTTON_NOW.style.color = "#000";
-  UI_ELEMENTS.BUTTON_NOW.style.border = "1px solid #000000";
-  UI_ELEMENTS.BUTTON_NOW.style.cursor = "pointer";
-
-  UI_ELEMENTS.BUTTON_DETAILS.style.background = "#cac7be";
-  UI_ELEMENTS.BUTTON_DETAILS.style.color = "#000";
-  UI_ELEMENTS.BUTTON_DETAILS.style.border = "1px solid #000000";
-  UI_ELEMENTS.BUTTON_DETAILS.style.cursor = "pointer";
-
-  UI_ELEMENTS.BUTTON_FORECAST.style.background = "#000";
-  UI_ELEMENTS.BUTTON_FORECAST.style.color = "#fff";
-  UI_ELEMENTS.BUTTON_FORECAST.style.border = "1px solid #000000";
-  UI_ELEMENTS.BUTTON_FORECAST.style.cursor = "pointer";
-}
-
-function renderNow(temperature, imgWeather, nameCity) {
-  UI_ELEMENTS.NOW_INFO_TEMPERATURE.textContent = temperature;
-  UI_ELEMENTS.NOW_IMG_WEATHER.src = imgWeather;
-  UI_ELEMENTS.NOW_CITY_NAME.textContent = nameCity;
-}
-
-function renderDetail(
-  nameCity,
-  temperature,
-  feelsLike,
-  weather,
-  sunrise,
-  sunset
-) {
-  UI_ELEMENTS.DETAIL_CITY_NAME.textContent = nameCity;
-  UI_ELEMENTS.DETAIL_LIST_TEMP.textContent = `Temperature: ${temperature}`;
-  UI_ELEMENTS.DETAIL_LIST_FEELS.textContent = `Feels like: ${feelsLike}`;
-  UI_ELEMENTS.DETAIL_LIST_WEATHER.textContent = `Weather: ${weather}`;
-  UI_ELEMENTS.DETAIL_LIST_SUNRISE.textContent = `Sunrise: ${sunrise}`;
-  UI_ELEMENTS.DETAIL_LIST_SUNSET.textContent = `Sunset: ${sunset}`;
-}
-
-function renderForecast(
-  nameCity,
-  date,
-  time,
-  temperature,
-  feelsLike,
-  textWeather,
-  imgWeather
-) {
-  const divContainer = CREATE_ELEMENTS.DIV();
-  const infoDate = CREATE_ELEMENTS.P();
-  const infoTemperature = CREATE_ELEMENTS.P();
-  const infoTime = CREATE_ELEMENTS.P();
-  const infoTextWeather = CREATE_ELEMENTS.P();
-  const infoImgWeather = CREATE_ELEMENTS.IMG();
-  const breakText = CREATE_ELEMENTS.BR();
-
-  divContainer.className = "page_three__info__card";
-  infoDate.className = "page_three__info__date";
-  infoTemperature.className = "page_three__info__temperature";
-  infoTime.className = "page_three__time";
-  infoTextWeather.className = "page_three__weather";
-  infoImgWeather.className = "page_three__card__sign";
-
-  UI_ELEMENTS.FORECAST_CITY_NAME.textContent = nameCity;
-  infoDate.textContent = date;
-  infoTemperature.textContent = `Temperature: ${temperature} Feels like: ${feelsLike}`;
-  infoTime.textContent = time;
-  infoTextWeather.textContent = textWeather;
-  infoImgWeather.src = imgWeather;
-
-  UI_ELEMENTS.FORECAST_CARD_LIST.append(divContainer);
-  divContainer.append(infoDate);
-  divContainer.append(infoTemperature);
-  divContainer.append(infoTime);
-  divContainer.append(infoTextWeather);
-  divContainer.append(infoImgWeather);
-  UI_ELEMENTS.FORECAST_CARD_LIST.append(breakText);
-}
-
-function updateAdedLocations(city, index, cityRender) {
-  const divCity = CREATE_ELEMENTS.DIV();
-  const nameCity = CREATE_ELEMENTS.A();
-  const deleteButton = CREATE_ELEMENTS.A();
-
-  divCity.id = index;
-  nameCity.className = "view_city";
-  nameCity.textContent = city.name;
-  deleteButton.className = "delete_city";
-  deleteButton.textContent = " ✕";
-
-  cityRender.append(divCity);
-  divCity.append(nameCity);
-  divCity.append(deleteButton);
+  divTask.append(nameTask);
+  nameTask.append(checkStatus);
+  nameTask.append(task.name);
+  divTask.append(deleteButton);
+  taskRender.append(divTask);
 
   deleteButton.addEventListener("click", function () {
-    deleteCity(divCity.id);
+    deleteTask(divTask.id);
   });
 
-  nameCity.addEventListener("click", function () {
-    cityFavoriteToLeftTab(city.name);
+  checkStatus.addEventListener("change", function () {
+    changeStatus(divTask.id);
+  });
+
+  if (task.status == STATUS.DONE) {
+    checkStatus.setAttribute("checked", true);
+  }
+}
+
+function render() {
+  UI_ELEMENTS.PUSH_PRIORITY_HIGHT.innerHTML = "";
+  UI_ELEMENTS.PUSH_PRIORITY_LOW.innerHTML = "";
+
+  taskList.forEach(function (task, index) {
+    if (task.priority === PRIORITY.HIGHT) {
+      const taskRender = UI_ELEMENTS.PUSH_PRIORITY_HIGHT;
+      pushTaskUi(task, index, taskRender);
+      UI_ELEMENTS.INPUT_FORM_HIGHT.reset();
+    } else if (task.priority === PRIORITY.LOW) {
+      const taskRender = UI_ELEMENTS.PUSH_PRIORITY_LOW;
+      pushTaskUi(task, index, taskRender);
+      UI_ELEMENTS.INPUT_FORM_LOW.reset();
+    }
   });
 }
 
-function renderAdedLocations() {
-  UI_ELEMENTS.CITY_LIST.innerHTML = "";
+UI_ELEMENTS.INPUT_FORM_HIGHT.addEventListener("submit", function (event) {
+  event.preventDefault();
+  addTask(UI_ELEMENTS.INPUT_TEXT_HIGHT.value, PRIORITY.HIGHT);
+});
 
-  cityFavoriteList.forEach(function (city, index) {
-    currentCity = UI_ELEMENTS.CITY_LIST;
-    updateAdedLocations(city, index, currentCity);
-  });
-
-  storage.saveCityFavoriteList(cityFavoriteList);
-}
-
-
-function convertKelvinToCelsius(temperature) {
-  return `${Math.round(temperature - 273.15)}°`;
-}
-
-function convertUnixToTime(time) {
-  const parseTime = new Date(time * 1000);
-  const hours = correctionTime(parseTime.getHours());
-  const minutes = correctionTime(parseTime.getMinutes());
-  const outputTime = `${hours}:${minutes}`;
-  return outputTime;
-}
-
-function convertUnixToDate(date) {
-  const parseDate = new Date(date * 1000);
-  const day = parseDate.getDate();
-  const month = parseDate.toLocaleString("en", { month: "long" });
-  const outputDate = `${day} ${month}`;
-  return outputDate;
-}
-
-function correctionTime(date) {
-  return date.toString().padStart(2, "0");
-}
+UI_ELEMENTS.INPUT_FORM_LOW.addEventListener("submit", function (event) {
+  event.preventDefault();
+  addTask(UI_ELEMENTS.INPUT_TEXT_LOW.value, PRIORITY.LOW);
+});
 
